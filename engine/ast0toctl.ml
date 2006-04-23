ccd1cde0cefd33eae079ed71e0a0eee7c381d941
Julia Lawall <julia.lawall@lip6.fr>
2006-04-23 08:27:51 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=414

--- engine/ast0toctl.ml
+++ engine/ast0toctl.ml
@@ -1,3 +1,6 @@
+(* Question: where do we put the existential quantifier for or.  At the
+moment, let it float inwards. *)
+
 module Ast0 = Ast0_cocci
 module Ast = Ast_cocci
 module CTL = Ast_ctl
@@ -12,19 +15,37 @@ type predicate =
   | Paren of string
   | Match of Ast.rule_elem
 
+let texify s =
+  let len = String.length s in
+  let rec loop n =
+    if n = len
+    then ""
+    else
+      match String.get s n with
+	'_' -> Printf.sprintf "\\_%s" (loop (n+1))
+      | '{' -> Printf.sprintf "{\\ttlb}%s" (loop (n+1))
+      | '}' -> Printf.sprintf "{\\ttrb}%s" (loop (n+1))
+      | '>' -> Printf.sprintf "\\mth{>}%s" (loop (n+1))
+      | c -> Printf.sprintf "%c%s" c (loop (n+1)) in
+  Printf.sprintf "\\mita{%s}" (loop 0)
+
 let pred2c = function
     State(s) -> "\\msf{State}("^s^")"
   | TrueBranch -> "\\msf{TrueBranch}"
   | FalseBranch -> "\\msf{FalseBranch}"
   | After -> "\\msf{After}"
   | Paren(s) -> "\\msf{Paren}("^s^")"
-  | Match(re) -> Unparse_cocci.rule_elem_to_string re
+  | Match(re) -> texify(Unparse_cocci.rule_elem_to_string re)
 
 (* --------------------------------------------------------------------- *)
 
 let get_option fn = function
     None -> None
   | Some x -> Some (fn x)
+
+let get_list_option fn = function
+    None -> []
+  | Some x -> fn x
 
 (* --------------------------------------------------------------------- *)
 (* --------------------------------------------------------------------- *)
@@ -120,8 +141,12 @@ let rec statement stmt after =
 	     (Ast.WhileHeader
 		(Ast0toast.mcode wh,Ast0toast.mcode lp,
 		 Ast0toast.expression exp,Ast0toast.mcode rp))) in
-      let body_line = CTL.Implies(CTL.Pred(TrueBranch),statement body after) in
-      make_seq while_header (Some body_line)
+      let body_line = CTL.Implies(CTL.Pred(TrueBranch),statement body None) in
+      let after_line =
+	match after with
+	  None -> CTL.True
+	| Some after -> CTL.Implies(CTL.Pred(FalseBranch),after) in
+      make_seq while_header (Some (CTL.And(body_line,after_line)))
   | Ast0.Return(ret,sem) ->
       make_seq
 	(CTL.Pred(Match(Ast.Return(Ast0toast.mcode ret,Ast0toast.mcode sem))))
@@ -180,49 +205,239 @@ let rec statement stmt after =
 	   (make_seq start_brace
 	      (Some(dots statement body (Some(make_seq end_brace after))))))
   | Ast0.OptStm(stm) ->
-      warning "arities not yet supported"; statement stm after
+      (* doesn't work for ?f(); f();, ie when the optional thing is the same
+	 as the thing that comes immediately after *)
+      let pattern = statement stm after in
+      (match after with
+	None -> CTL.Or(pattern,CTL.Not pattern)
+      |	Some after ->
+	  CTL.Or(pattern,CTL.And(CTL.Not(statement stm None),after)))
   | Ast0.UniqueStm(stm) ->
       warning "arities not yet supported"; statement stm after
   | Ast0.MultiStm(stm) ->
       warning "arities not yet supported"; statement stm after
   | _ -> failwith "not supported"
 
+(* --------------------------------------------------------------------- *)
+
+let fvdots fn = function
+    Ast.DOTS(l) | Ast.CIRCLES(l) | Ast.STARS(l) ->
+      List.fold_left Common.union_set [] (List.map fn l)
+
+let mcode (x,_) = x
+
+let rec fvident = function
+    Ast.Id(name) -> []
+  | Ast.MetaId(name) -> [mcode name]
+  | Ast.MetaFunc(name) -> [mcode name]
+  | Ast.MetaLocalFunc(name) -> [mcode name]
+  | Ast.OptIdent(id) -> fvident id
+  | Ast.UniqueIdent(id) -> fvident id
+  | Ast.MultiIdent(id) -> fvident id
+
+let rec fvexpr = function
+    Ast.Ident(id) -> ((fvident id) : string list)
+  | Ast.Constant(const) -> []
+  | Ast.FunCall(fn,lp,args,rp) ->
+      Common.union_set (fvexpr fn) (fvdots fvexpr args)
+  | Ast.Assignment(left,op,right) ->
+      Common.union_set (fvexpr left) (fvexpr right)
+  | Ast.CondExpr(exp1,why,exp2,colon,exp3) ->
+      Common.union_set (fvexpr exp1)
+	(Common.union_set (get_list_option fvexpr exp2) (fvexpr exp3))
+  | Ast.Postfix(exp,op) -> fvexpr exp
+  | Ast.Infix(exp,op) -> fvexpr exp
+  | Ast.Unary(exp,op) -> fvexpr exp
+  | Ast.Binary(left,op,right) ->
+      Common.union_set (fvexpr left) (fvexpr right)
+  | Ast.Paren(lp,exp,rp) -> fvexpr exp
+  | Ast.ArrayAccess(exp1,lb,exp2,rb) ->
+      Common.union_set (fvexpr exp1) (fvexpr exp2)
+  | Ast.RecordAccess(exp,pt,field) ->
+      Common.union_set (fvexpr exp) (fvident field)
+  | Ast.RecordPtAccess(exp,ar,field) ->
+      Common.union_set (fvexpr exp) (fvident field)
+  | Ast.Cast(lp,ty,rp,exp) -> Common.union_set (fvtypeC ty) (fvexpr exp)
+  | Ast.MetaConst(name,_) -> [mcode name]
+  | Ast.MetaErr(name) -> [mcode name]
+  | Ast.MetaExpr(name,_) -> [mcode name]
+  | Ast.MetaExprList(name) -> [mcode name]
+  | Ast.EComma(cm) -> []
+  | Ast.DisjExpr(exp_list) ->
+      List.fold_left Common.union_set [] (List.map fvexpr exp_list)
+  | Ast.NestExpr(expr_dots) -> fvdots fvexpr expr_dots
+  | Ast.Edots(dots,Some whencode)
+  | Ast.Ecircles(dots,Some whencode)
+  | Ast.Estars(dots,Some whencode) -> fvexpr whencode
+  | Ast.Edots(dots,None) | Ast.Ecircles(dots,None)
+  | Ast.Estars(dots,None) -> []
+  | Ast.OptExp(exp) -> fvexpr exp
+  | Ast.UniqueExp(exp) -> fvexpr exp
+  | Ast.MultiExp(exp) -> fvexpr exp
+
+and fvtypeC = function
+    Ast.BaseType(ty,_) -> []
+  | Ast.Pointer(ty,star) -> fvtypeC ty
+  | Ast.Array(ty,lb,None,rb) -> fvtypeC ty
+  | Ast.Array(ty,lb,Some size,rb) ->
+      Common.union_set (fvtypeC ty) (fvexpr size)
+  | Ast.StructUnionName(name,kind) -> []
+  | Ast.TypeName(name)-> []
+  | Ast.MetaType(name)-> [mcode name]
+  | Ast.OptType(ty) -> fvtypeC ty
+  | Ast.UniqueType(ty) -> fvtypeC ty
+  | Ast.MultiType(ty) -> fvtypeC ty
+
+let rec fvdeclaration = function
+    Ast.Init(ty,id,eq,exp,sem) ->
+      Common.union_set (fvtypeC ty)
+	(Common.union_set (fvident id) (fvexpr exp))
+  | Ast.UnInit(ty,id,sem) -> Common.union_set (fvtypeC ty) (fvident id)
+  | Ast.OptDecl(decl) -> fvdeclaration decl
+  | Ast.UniqueDecl(decl) -> fvdeclaration decl
+  | Ast.MultiDecl(decl) -> fvdeclaration decl
+
+let rec fvparameterTypeDef = function
+    Ast.VoidParam(ty) -> fvtypeC ty
+  | Ast.Param(id,_,ty) -> Common.union_set (fvtypeC ty) (fvident id)
+  | Ast.MetaParam(name) -> [mcode name]
+  | Ast.MetaParamList(name) -> [mcode name]
+  | Ast.PComma(cm) -> []
+  | Ast.Pdots(dots) | Ast.Pcircles(dots) -> []
+  | Ast.OptParam(param) -> fvparameterTypeDef param
+  | Ast.UniqueParam(param) -> fvparameterTypeDef param
+
+let fvparameter_list = fvdots fvparameterTypeDef
+
+let rec fvrule_elem = function
+    Ast.FunDecl(stg,name,lp,params,rp) -> fvparameter_list params
+  | Ast.Decl(decl) -> fvdeclaration decl
+  | Ast.SeqStart(brace) -> []
+  | Ast.SeqEnd(brace) -> []
+  | Ast.ExprStatement(exp,sem) -> fvexpr exp
+  | Ast.IfHeader(iff,lp,exp,rp) -> fvexpr exp
+  | Ast.Else(els) -> []
+  | Ast.WhileHeader(whl,lp,exp,rp) -> fvexpr exp
+  | Ast.Do(d) -> []
+  | Ast.WhileTail(whl,lp,exp,rp,sem) -> fvexpr exp
+  | Ast.ForHeader(fr,lp,e1,sem1,e2,sem2,e3,rp) ->
+      Common.union_set (get_list_option fvexpr e1)
+	(Common.union_set (get_list_option fvexpr e2)
+	   (get_list_option fvexpr e3))
+  | Ast.Return(ret,sem) -> []
+  | Ast.ReturnExpr(ret,exp,sem) -> fvexpr exp
+  | Ast.MetaStmt(name) -> [mcode name]
+  | Ast.MetaStmtList(name) -> [mcode name]
+  | Ast.Disj(rule_elem_dots_list) ->
+      List.fold_left Common.union_set []
+	(List.map (function x -> fvdots fvrule_elem x) rule_elem_dots_list)
+  | Ast.Nest(rule_elem_dots) -> fvdots fvrule_elem rule_elem_dots
+  | Ast.Exp(exp) -> fvexpr exp
+  | Ast.Dots(dots,None) | Ast.Circles(dots,None) | Ast.Stars(dots,None) -> []
+  | Ast.Dots(d,Some x) | Ast.Circles(d,Some x) | Ast.Stars(d,Some x) ->
+      fvdots fvrule_elem x
+  | Ast.OptRuleElem(re) ->
+      List.fold_left Common.union_set [] (List.map fvrule_elem re)
+  | Ast.UniqueRuleElem(re) ->
+      List.fold_left Common.union_set [] (List.map fvrule_elem re)
+  | Ast.MultiRuleElem(re) ->
+      List.fold_left Common.union_set [] (List.map fvrule_elem re)
+
+let free_table =
+  (Hashtbl.create(50) :
+     ((predicate,string) CTL.generic_ctl,string list) Hashtbl.t)
+
+let rec free_vars x =
+  let res =
+    match x with
+      CTL.False -> []
+    | CTL.True -> []
+    | CTL.Pred(Match(p)) -> fvrule_elem p
+    | CTL.Pred(Paren(p)) -> [p]
+    | CTL.Pred(p) -> []
+    | CTL.Not(f) -> free_vars f
+    | CTL.Exists(vars,f) -> free_vars f
+    | CTL.And(f1,f2) -> Common.union_set (free_vars f1) (free_vars f2)
+    | CTL.Or(f1,f2) -> Common.union_set (free_vars f1) (free_vars f2)
+    | CTL.Implies(f1,f2) -> Common.union_set (free_vars f1) (free_vars f2)
+    | CTL.AF(f) -> free_vars f
+    | CTL.AX(f) -> free_vars f
+    | CTL.AG(f) -> free_vars f
+    | CTL.AU(f1,f2) -> Common.union_set (free_vars f1) (free_vars f2)
+    | CTL.EF(f) -> free_vars f
+    | CTL.EX(f) -> free_vars f
+    | CTL.EG(f) -> free_vars f
+    | CTL.EU(f1,f2) -> Common.union_set (free_vars f1) (free_vars f2) in
+  Hashtbl.add free_table x res;
+  res
+
+(* --------------------------------------------------------------------- *)
+
+let add_quants formula variables =
+  List.fold_left (function prev -> function v -> CTL.Exists (v,prev))
+    formula variables
+
 let rec add_quantifiers quantified = function
     CTL.False -> CTL.False
   | CTL.True -> CTL.True
-  | CTL.Pred(Match(p)) ->  of 'a
+  | (CTL.Pred(Match(p))) as x ->
+      let vars = Hashtbl.find free_table x in
+      let fresh =
+	List.filter (function x -> not (List.mem x quantified)) vars in
+      add_quants (CTL.Pred(Match(p))) fresh
   | CTL.Pred(p) -> CTL.Pred(p)
   | CTL.Not(f) -> CTL.Not(add_quantifiers quantified f)
-  | CTL.Exists(vars,f) -> CTL.Exists(vars,add_quantifiers (vars@quantified) f)
+  | CTL.Exists(vars,f) -> CTL.Exists(vars,add_quantifiers quantified f)
   | CTL.And(f1,f2) ->
+      binop f1 f2 quantified (function x -> function y -> CTL.And(x,y))
   | CTL.Or(f1,f2) ->
+      CTL.Or(add_quantifiers quantified f1,add_quantifiers quantified f2)
   | CTL.Implies(f1,f2) ->
+      binop f1 f2 quantified (function x -> function y -> CTL.Implies(x,y))
   | CTL.AF(f) -> CTL.AF(add_quantifiers quantified f)
   | CTL.AX(f) -> CTL.AX(add_quantifiers quantified f)
   | CTL.AG(f) -> CTL.AG(add_quantifiers quantified f)
   | CTL.AU(f1,f2) ->
-      CTL.AU(add_quantifiers quantified f1,add_quantifiers quantified f2)
+      binop f1 f2 quantified (function x -> function y -> CTL.AU(x,y))
   | CTL.EF(f) -> CTL.EF(add_quantifiers quantified f)
   | CTL.EX(f) -> CTL.EX(add_quantifiers quantified f)
   | CTL.EG(f) -> CTL.EG(add_quantifiers quantified f)
   | CTL.EU(f1,f2) ->
-      CTL.EU(add_quantifiers quantified f1,add_quantifiers quantified f2)
-	
+      binop f1 f2 quantified (function x -> function y -> CTL.EU(x,y))
+
+and binop f1 f2 quantified fn =
+  let f1vars = Hashtbl.find free_table f1 in
+  let f2vars = Hashtbl.find free_table f2 in
+  let common = Common.inter_set f1vars f2vars in
+  let fresh = Common.minus_set common quantified in
+  let now_quantified = fresh @ quantified in
+  add_quants
+    (fn
+       (add_quantifiers now_quantified f1)
+       (add_quantifiers now_quantified f2))
+    fresh
+
 (* --------------------------------------------------------------------- *)
 (* Function declaration *)
-(* Haven't thought much about arity here... *)
 
 let top_level = function
     Ast0.DECL(decl) -> failwith "not supported"
   | Ast0.INCLUDE(inc,s) -> failwith "not supported"
   | Ast0.FILEINFO(old_file,new_file) -> failwith "not supported"
-  | Ast0.FUNCTION(stmt) -> add_quantifiers [] (statement stmt None)
+  | Ast0.FUNCTION(stmt) ->
+      let start = statement stmt None in
+      let _ = free_vars start in
+      add_quantifiers [] start
   | Ast0.CODE(rule_elem_dots) ->
-      add_quantifiers [] (dots statement rule_elem_dots None)
+      let start = dots statement rule_elem_dots None in
+      let _ = free_vars start in
+      add_quantifiers [] start
   | Ast0.ERRORWORDS(exps) -> failwith "not supported"
   | Ast0.OTHER(_) -> failwith "eliminated by top_level"
 
 (* --------------------------------------------------------------------- *)
 (* Entry points *)
 
-let ast0toctl = List.map top_level
+let ast0toctl l =
+  ctr := 0;
+  List.map top_level l
--- engine/ctltotex.ml
+++ engine/ctltotex.ml
@@ -17,39 +17,116 @@ let prelude =
   "\\newcommand{\\msf}[1]{\\mbox{\\sf{{#1}}}}\n"^
   "\\newcommand{\\mrm}[1]{\\mbox{\\rm{{#1}}}}\n"^
   "\\newcommand{\\mth}[1]{\\({#1}\\)}\n\n"^
+  "\\newcommand{\\ttlb}{\\mbox{\\tt \\char'173}}\n"^
+  "\\newcommand{\\ttrb}{\\mbox{\\tt \\char'175}}\n\n"^
   "\\begin{document}\n"
 
 let postlude = "\\end{document}"
 
-let rec ctl2c pp pv = function
-    CTL.False -> "\\msf{false}"
-  | CTL.True -> "\\msf{true}"
-  | CTL.Pred(p) -> pp p
-  | CTL.Not(f) -> "\\neg "^(wrap pp pv f)
-  | CTL.Exists(v,f) -> "\\exists "^(pv v)^" . "^(ctl2c pp pv f)
-  | CTL.And(f1,f2) -> (wrap pp pv f1)^" \\wedge "^(wrap pp pv f2)
-  | CTL.Or(f1,f2) -> (wrap pp pv f1)^" \\vee "^(wrap pp pv f2)
-  | CTL.Implies(f1,f2) -> (wrap pp pv f1)^" \\rightarrow\n"^(wrap pp pv f2)
-  | CTL.AF(f) -> "\\AF"^(pathwrap pp pv f)
-  | CTL.AX(f) -> "\\AX"^(pathwrap pp pv f)
-  | CTL.AG(f) -> "\\AG"^(pathwrap pp pv f)
-  | CTL.AU(f1,f2) -> "\\A["^(ctl2c pp pv f1)^" \\U "^(ctl2c pp pv f2)^"]\n"
-  | CTL.EF(f) -> "\\EF"^(pathwrap pp pv f)
-  | CTL.EX(f) -> "\\EX"^(pathwrap pp pv f)
-  | CTL.EG(f) -> "\\EG"^(pathwrap pp pv f)
-  | CTL.EU(f1,f2) -> "\\E["^(ctl2c pp pv f1)^" \\U "^(ctl2c pp pv f2)^"]\n"
+let check_ct ct res = if ct > 80 then (res^"\\\\",0) else (res,ct)
+let texify s =
+  let len = String.length s in
+  let rec loop n =
+    if n = len
+    then ""
+    else
+      match String.get s n with
+	'_' -> Printf.sprintf "\\_%s" (loop (n+1))
+      | c -> Printf.sprintf "%c%s" c (loop (n+1)) in
+  Printf.sprintf "\\mita{%s}" (loop 0)
 
-and wrap pp pv x =
+let rec ctl2c ct pp pv = function
+    CTL.False -> ("\\msf{false}",5)
+  | CTL.True -> ("\\msf{true}",4)
+  | CTL.Pred(p) ->
+      let res = pp p in
+      (res,ct+String.length res)
+  | CTL.Not(f) ->
+      let (res,ct) = wrap (ct+1) pp pv f in
+      ("\\neg "^res,ct)
+  | CTL.Exists(v,f) ->
+      let res1 = texify(pv v) in
+      let ct = ct + String.length res1 in
+      let (res1,ct) = check_ct ct res1 in
+      let (res2,ct) = existswrap (ct+1) pp pv f in
+      ("\\exists "^res1^" . "^res2,ct)
+  | CTL.And(f1,f2) ->
+      let (res1,ct) = andwrap ct pp pv f1 in
+      let (res1,ct) = check_ct ct res1 in
+      let (res2,ct) = andwrap (ct+1) pp pv f2 in
+      (res1^" \\wedge "^res2,ct)
+  | CTL.Or(f1,f2) ->
+      let (res1,ct) = orwrap ct pp pv f1 in
+      let (res1,ct) = check_ct ct res1 in
+      let (res2,ct) = orwrap (ct+1) pp pv f2 in
+      (res1^" \\vee "^res2,ct)
+  | CTL.Implies(f1,f2) ->
+      let (res1,ct) = wrap ct pp pv f1 in
+      let (res1,ct) = check_ct ct res1 in
+      let (res2,ct) = wrap (ct+1) pp pv f2 in
+      (res1^" \\rightarrow "^res2,ct)
+  | CTL.AF(f) ->
+      let (res,ct) = pathwrap (ct+2) pp pv f in ("\\AF"^res,ct)
+  | CTL.AX(f) ->
+      let (res,ct) = pathwrap (ct+2) pp pv f in ("\\AX"^res,ct)
+  | CTL.AG(f) ->
+      let (res,ct) = pathwrap (ct+2) pp pv f in ("\\AG"^res,ct)
+  | CTL.AU(f1,f2) ->
+      let (res1,ct) = existswrap (ct+2) pp pv f1 in
+      let (res1,ct) = check_ct ct res1 in
+      let (res2,ct) = existswrap (ct+3) pp pv f2 in
+      ("\\A["^res1^" \\U "^res2^"]\n",ct)
+  | CTL.EF(f) ->
+      let (res,ct) = pathwrap (ct+2) pp pv f in ("\\EF"^res,ct)
+  | CTL.EX(f) ->
+      let (res,ct) = pathwrap (ct+2) pp pv f in ("\\EX"^res,ct)
+  | CTL.EG(f) ->
+      let (res,ct) = pathwrap (ct+2) pp pv f in ("\\EG"^res,ct)
+  | CTL.EU(f1,f2) ->
+      let (res1,ct) = existswrap (ct+2) pp pv f1 in
+      let (res1,ct) = check_ct ct res1 in
+      let (res2,ct) = existswrap (ct+3) pp pv f2 in
+      ("\\E["^res1^" \\U "^res2^"]\n",ct)
+
+and wrap ct pp pv x =
   match x with
-    CTL.False | CTL.True | CTL.Pred(_) -> ctl2c pp pv x
-  | _ -> Printf.sprintf "(%s)" (ctl2c pp pv x)
+    CTL.False | CTL.True | CTL.Pred(_) -> ctl2c ct pp pv x
+  | _ ->
+      let (res,ct) = ctl2c (ct+1) pp pv x in
+      (Printf.sprintf "(%s)" res,ct+1)
 
-and pathwrap pp pv x =
+and andwrap ct pp pv x =
+  match x with
+    CTL.And(_,_) | CTL.False | CTL.True | CTL.Pred(_) -> ctl2c ct pp pv x
+  | _ ->
+      let (res,ct) = ctl2c (ct+1) pp pv x in
+      (Printf.sprintf "(%s)" res,ct+1)
+
+and orwrap ct pp pv x =
+  match x with
+    CTL.Or(_,_) | CTL.False | CTL.True | CTL.Pred(_) -> ctl2c ct pp pv x
+  | _ ->
+      let (res,ct) = ctl2c (ct+1) pp pv x in
+      (Printf.sprintf "(%s)" res,ct+1)
+
+and pathwrap ct pp pv x =
   match x with
     CTL.AX(_) | CTL.AF(_) | CTL.AG(_) | CTL.AU(_,_)
   | CTL.EX(_) | CTL.EF(_) | CTL.EG(_) | CTL.EU(_,_) ->
-      ctl2c pp pv x
-  | _ -> Printf.sprintf "(%s)" (ctl2c pp pv x)
+      ctl2c ct pp pv x
+  | _ ->
+      let (res,ct) = ctl2c (ct+1) pp pv x in
+      (Printf.sprintf "(%s)" res,ct+1)
+
+and existswrap ct pp pv x =
+  match x with
+    CTL.AX(_) | CTL.AF(_) | CTL.AG(_) | CTL.AU(_,_) | CTL.Pred(_)
+  | CTL.EX(_) | CTL.EF(_) | CTL.EG(_) | CTL.EU(_,_) | CTL.Exists(_,_)
+  | CTL.True | CTL.False | CTL.Not(_) ->
+      ctl2c ct pp pv x
+  | _ ->
+      let (res,ct) = ctl2c (ct+1) pp pv x in
+      (Printf.sprintf "(%s)" res,ct+1)
 
 let ctltotex rule pp pv ctls o =
   Printf.fprintf o "\\begin{quote}\\begin{verbatim}\n";
@@ -57,10 +134,11 @@ let ctltotex rule pp pv ctls o =
   Printf.fprintf o "\\end{verbatim}\\end{quote}\n\n";
   List.iter
     (function ctl ->
-      Printf.fprintf o "\\[\n";
-      Printf.fprintf o "%s\n" (ctl2c pp pv ctl))
+      Printf.fprintf o "\\[\\begin{array}{l}\n";
+      let (res,_) = ctl2c 0 pp pv ctl in
+      Printf.fprintf o "%s\n" res)
     ctls;
-  Printf.fprintf o "\\]\n\n"
+  Printf.fprintf o "\\end{array}\\]\n\n"
   
 let make_prelude o = Printf.fprintf o "%s\n" prelude
 let make_postlude o = Printf.fprintf o "%s\n" postlude
