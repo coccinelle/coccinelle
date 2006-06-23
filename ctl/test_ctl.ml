29246d77bd6704d0d16fbe6e27879a402cdae396
Rene Rydhof Hansen <rrh@cs.aau.dk>
2006-06-23 14:49:04 +0000

    - Added module defining predicates a string - Changed examples etc. to use new structure - Added details to example
    svn path=/coccinelle/; revision=542

--- ctl/test_ctl.ml
+++ ctl/test_ctl.ml
@@ -1,11 +1,13 @@
-
 (* ********************************************************************** *)
 (* Module: EXAMPLE_ENGINE (instance of CTL_ENGINE)                        *)
 (* ********************************************************************** *)
 
-(* FIX ME: move *)
-module EXAMPLE_ENGINE = Ctl_engine.CTL_ENGINE (Ctl_engine.SIMPLE_ENV) (Ctl_engine.SIMPLE_CFG);;
+module WRAPPER_PRED = 
+  struct
+    type predicate = string
+  end
 
+module EXAMPLE_ENGINE = Wrapper_ctl.CTL_ENGINE_BIS (Ctl_engine.SIMPLE_ENV) (Ctl_engine.SIMPLE_CFG) (WRAPPER_PRED)
 
 let top_wit = []
 
@@ -17,16 +19,18 @@ let top_wit = []
 
 (* For convenience in the examples *)
 (* FIX ME: remove *)
-open Ctl_engine.SIMPLE_ENV;;
 open Ctl_engine.SIMPLE_CFG;;
 open EXAMPLE_ENGINE;;
+open Ast_ctl;;
 
 (* ---------------------------------------------------------------------- *)
 (* Helpers                                                                *)
 (* ---------------------------------------------------------------------- *)
 
+(* FIX ME: move to ENGINE module *)
 let (-->) x v = Subst (x,v);;
 
+(* FIX ME: move to ENGINE module *)
 let (-/->) x v = NegSubst(x,v);;
 
 let mkgraph nodes edges = 
@@ -53,19 +57,6 @@ let mkgraph nodes edges =
 (*   CTL: f(x) /\ AF(Ey.g(y))                                           *)
 (* ******************************************************************** *)
 
-(*
-let conv_label olab (p,mv) =
-  let old_label = olab p in
-  let penv = mv --> PredVal(p) in
-  let conv_sub sub =
-    match sub with
-      | Subst(x,v)    -> Subst(UnModif(x),CodeVal(v))
-      | NegSubst(x,v) -> NegSubst(UnModif(x),CodeVal(v))
-  in
-    List.map (fun (s,env,wit) -> (s,penv :: (List.map conv_sub env),wit)) old_label
-;;
-*)
-
 let ex1lab s =
   match s with
     "f(x)" -> [(0,["x" --> "1"],top_wit); (1,["x" --> "2"],top_wit)]
@@ -82,11 +73,11 @@ let ex1graph =
 
 let ex1states = List.map fst (ex1graph#nodes)#tolist;;
 
-let ex1model = (ex1graph,(*conv_label*) ex1lab,ex1states);;
+let ex1model = (ex1graph,ex1lab,ex1states);;
 
-let ex1s0 = Exists((*UnModif*) "v0",Pred ("f(x)",UnModif "v0"));;
-let ex1s1 = Exists((*UnModif*) "v1",Pred ("g(y)",UnModif "v1"));;
-let ex1s2 = Exists((*UnModif*) "y",ex1s1);;
+let ex1s0 = Exists("v0",Pred ("f(x)",UnModif "v0"));;
+let ex1s1 = Exists("v1",Pred ("g(y)",Modif "v1"));;
+let ex1s2 = Exists("y",ex1s1);;
 let ex1s3 = AF(ex1s2);;
 let ex1s4 = And(ex1s0,ex1s3);;
 
@@ -94,9 +85,56 @@ let ex1s3a = AX(ex1s2);;
 let ex1s4a = AX(AX(ex1s2));;
 let ex1s5a = And(ex1s0,ex1s4a);;
 
+let ex1s0b = Pred ("g(y)", Modif "v0");;
+let ex1s1b = Exists ("v0",ex1s0b);;
+let ex1s2b = Exists ("y",ex1s1b);;
+let ex1s3b = AF(ex1s2b);;
+let ex1s4b = AX(ex1s3b);;
+let ex1s5b = Pred ("f(x)", UnModif "v3");;
+let ex1s6b = Exists ("v3", ex1s5b);;
+let ex1s7b = Exists ("x", ex1s6b);;
+let ex1s8b = And(ex1s7b,ex1s4b);;
+
+let ex1s7c = And(ex1s6b,ex1s4b);;
+let ex1s8c = Exists("x",ex1s7c);;
+
 let ex1phi1 = ex1s4;;
 let ex1phi2 = ex1s5a;;
+let ex1phi3 = 
+ And
+ (Exists ("x",
+  (Exists ("v3",
+    Pred ("f(x)", UnModif "v3")))),
+  AX
+   (AF
+    (Exists ("y", (* change this to Y and have strange behaviour *)
+      (Exists ("v0",
+       Pred ("g(y)", Modif "v0")
+                      ))))));;
+
+(*
+     [(3, [("x","1");("y","1")], "g(y)");
+      (4, [("x","1");("y","2")], "g(y)");
+      (3, [("x","2");("y","1")], "g(y)");
+      (4, [("x","2");("y","2")], "g(y)");
 
 
-let ex1 phi = sat ex1model phi;;
+[(0, [],
+  [Wit (0, [Subst ("x", Wrapper_ctl.ClassicVar "1")], [],
+    [Wit (0, [Subst ("v3", Wrapper_ctl.PredVar (UnModif "f(x)"))], [], [])]);
+   Wit (3, [Subst ("y", Wrapper_ctl.ClassicVar "1")], [],
+    [Wit (3, [Subst ("v0", Wrapper_ctl.PredVar (Modif "g(y)"))], [], [])]);
+   Wit (4, [Subst ("y", Wrapper_ctl.ClassicVar "2")], [],
+    [Wit (4, [Subst ("v0", Wrapper_ctl.PredVar (Modif "g(y)"))], [], [])])]);
+ (1, [],
+  [Wit (1, [Subst ("x", Wrapper_ctl.ClassicVar "2")], [],
+    [Wit (1, [Subst ("v3", Wrapper_ctl.PredVar (UnModif "f(x)"))], [], [])]);
+   Wit (3, [Subst ("y", Wrapper_ctl.ClassicVar "1")], [],
+    [Wit (3, [Subst ("v0", Wrapper_ctl.PredVar (Modif "g(y)"))], [], [])]);
+   Wit (4, [Subst ("y", Wrapper_ctl.ClassicVar "2")], [],
+    [Wit (4, [Subst ("v0", Wrapper_ctl.PredVar (Modif "g(y)"))], [], [])])])]
+*)
 
+
+
+(* let ex1 phi = satbis ex1model phi;; *)
