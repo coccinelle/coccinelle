6024a073e616aa37654e67e1e3183a9d7600d1e8
Rene Rydhof Hansen <rrh@cs.aau.dk>
2006-06-29 10:20:08 +0000

    - Added definition of modules used for examples - Added new example
    svn path=/coccinelle/; revision=583

--- ctl/test_ctl.ml
+++ ctl/test_ctl.ml
@@ -1,14 +1,28 @@
+(* -*- mode: tuareg; tuareg-interactive-program: "/home/rrh/coccinelle/coccinelle/ctl/ctl.top -I ../commons" -*- *)
+
 (* ********************************************************************** *)
 (* Module: EXAMPLE_ENGINE (instance of CTL_ENGINE)                        *)
 (* ********************************************************************** *)
 
+(* Simple env.: meta.vars and values are strings *)
+module SIMPLE_ENV =
+  struct
+    type value = string;;
+    type mvar = string;;
+    let eq_mvar x x' = x = x';;
+    let eq_val v v' = v = v';;
+    let merge_val v v' = v;;
+  end
+;;
+
+(* Simple predicates *)
 module WRAPPER_PRED = 
   struct
     type predicate = string
   end
 
 module EXAMPLE_ENGINE = 
-  Wrapper_ctl.CTL_ENGINE_BIS (Ctl_engine.SIMPLE_ENV) (Ctl_engine.SIMPLE_CFG) (WRAPPER_PRED)
+  Wrapper_ctl.CTL_ENGINE_BIS (SIMPLE_ENV) (Ctl_engine.OGRAPHEXT_GRAPH) (WRAPPER_PRED)
 
 let top_wit = []
 
@@ -20,7 +34,7 @@ let top_wit = []
 
 (* For convenience in the examples *)
 (* FIX ME: remove *)
-open Ctl_engine.SIMPLE_CFG;;
+open Ctl_engine.OGRAPHEXT_GRAPH;;
 open EXAMPLE_ENGINE;;
 open Ast_ctl;;
 
@@ -54,15 +68,19 @@ let mkgraph nodes edges =
 
 
 (* ******************************************************************** *)
-(* NEW Example 1                                                        *)
+(* Example 1                                                            *)
 (*   CTL: f(x) /\ AF(Ey.g(y))                                           *)
 (* ******************************************************************** *)
 
 let ex1lab s =
   match s with
-    "f(x)" -> [(0,["x" --> "1"]); (1,["x" --> "2"])]
-  | "g(y)" -> [(3,["y" --> "1"]); (4,["y" --> "2"])]
-  | _ -> []
+    | "f(x)" -> [(0,["x" --> "1"]); (1,["x" --> "2"])]
+    | "g(y)" -> [(3,["y" --> "1"]); (4,["y" --> "2"])]
+    | "f(1)" -> [(0,[])]
+    | "f(2)" -> [(1,[])]
+    | "g(1)" -> [(3,[])]
+    | "g(2)" -> [(4,[])]
+    | _ -> []
 ;;
 
 let ex1graph = 
@@ -75,6 +93,7 @@ let ex1graph =
 let ex1states = List.map fst (ex1graph#nodes)#tolist;;
 
 let ex1model = (ex1graph,ex1lab,ex1states);;
+let ex1model_wrapped = (ex1graph,wrap_label ex1lab,ex1states);;
 
 let ex1s0 = Exists("v0",Pred ("f(x)",UnModif "v0"));;
 let ex1s1 = Exists("v1",Pred ("g(y)",Modif "v1"));;
@@ -126,5 +145,91 @@ let ex1phi4 =
 				  )))))));;
 
 
+let ex1phi5 = AU(True,Exists("y", Exists("v0",Pred("g(y)",Modif "v0"))));;
+
+let ex1phi6 = 
+  AU(
+    Not(Exists("x",Exists("v1",Pred("f(x)",UnModif "v1")))),
+    Exists("y", Exists("v0",Pred("g(y)",Modif "v0")))
+  );;
+
+(* use with ex1nc *)
+let ex1phi7 = 
+  AU(
+    Not(Or(Pred("f(1)",Control),Pred("f(2)",Control))),
+    Exists("y", Exists("v0",Pred("g(y)",Modif "v0")))
+  );;
 
 let ex1 phi = satbis ex1model phi;;
+let ex1nc phi = satbis_noclean ex1model phi;;
+
+
+(* ******************************************************************** *)
+(* Example 2                                                            *)
+(* ******************************************************************** *)
+
+let ex2lab s =
+  match s with
+    | "p"        -> [0,[]]
+    | "{"        -> [(1,[]); (2,[])]
+    | "}"        -> [(3,[]); (4,[])]
+    | "paren(v)" -> [(1,["v" --> "1"]); (2,["v" --> "2"]); 
+		     (3,["v" --> "2"]); (4,["v" --> "1"])]
+    | _          -> []
+;;
+
+let ex2graph = 
+  let nodes = 
+    [(0,"p");(1,"{");(2,"{");(3,"}");(4,"}");(5,"<exit>")] in
+  let edges = [(0,1); (1,2); (2,3); (3,4); (4,5); (5,5)] in
+  mkgraph nodes (List.map (fun (x,y) -> (x,y,())) edges)
+;;
+
+let ex2states = List.map fst (ex2graph#nodes)#tolist;;
+
+let ex2model = (ex2graph,ex2lab,ex2states);;
+let ex2model_wrapped = (ex2graph,wrap_label ex2lab,ex2states);;
+
+let ex2s0 = Pred("p",Control);;
+let ex2s1 = Pred("{",Control);;
+let ex2s2 = Pred("paren(v)",Control);;
+let ex2s3 = And(ex2s1,ex2s2);;
+let ex2s4 = Pred("}",Control);;
+let ex2s5 = Pred("paren(v)",Control);;
+let ex2s6 = And(ex2s4,ex2s5);;
+let ex2s7 = AF(ex2s6);;
+let ex2s8 = And(ex2s3,ex2s7);;
+let ex2s9 = Exists("v",ex2s8);;
+let ex2s10 = AX(ex2s9);;
+let ex2s11 = And(ex2s0,ex2s10);;
+
+let ex2phi1 = ex2s11;;
+
+(*
+           +--- s11:& ---+
+           |             |
+         s0:p         s10:AX
+                         |
+                     s9:exists v
+                         |
+           +---------- s8:& --------+
+           |                        |
+     +-- s3:& --+                 s7:AF
+     |          |                   |
+   s1:"{"     s2:paren(v)     +--  s6:& -+
+                              |          |
+		            s4:"}"     s5:paren(v)
+
+  s0 : p                   : (0,_,_)
+  s1 : "{"                 : (1,_,_); (2,_,_)
+  s2 : paren(v)            : (1,v=1,_); (2,v=2,_); (3,v=2,_); (4,v=1,_)
+  s3 : "{" & paren(v)      : (1,v=1,_); (2,v=2,_)
+  s4 : "}"                 : (3,_,_); (4,_,_)
+  s5 : paren(v)            : (1,v=1,_); (2,v=2,_); (3,v=2,_); (4,v=1,_)
+  s6 : "}" & paren(v)      : (3,v=2,_); (4,v=1,_)
+  s7 : AF(...)             : (0;1;2;3,v=2,_); (0;1;2;3;4,v=1,_)
+  s8 : (...&...) & AF(...) : (1,v=1,_); (2,v=2,_)
+  s9 : exists ...          : (1,_,(1,v=1)); (2,_,(2,v=2))
+ s10 : AX(...)             : (0,_,(1,v=1)); (1,_,(2,v=2))
+ s11 : p & AX(...)         : (0,_,(1,v=1))
+*)
