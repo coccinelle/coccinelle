97e7d730013e9271ee86cf8da2b65dd9fc53814b
Yoann Padioleau <pad@cs.uiuc.edu>
2006-06-29 20:22:27 +0000

    more test files
    svn path=/coccinelle/; revision=599

---
+++ tests/test6.c
@@ -0,0 +1,26 @@
+int i;
+
+void main()
+{
+  /* a comment */
+
+  f(4) + f(5);
+
+  //f(f(3)); // if uncomment, should have the warning "already minused token"
+  if(f(1))
+    f(1);
+  else
+    f(2);
+
+  if(1) 
+    g(1);
+  else 
+    g(2);
+}
+
+
+void mainbis() {
+  f(10);
+}
+
+
