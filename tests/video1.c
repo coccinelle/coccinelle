5ddea122a3c678b0d3cb38888ba6d6b5539711d9
Julia Lawall <julia.lawall@lip6.fr>
2006-07-21 15:35:36 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=702

--- tests/video1.c
+++ tests/video1.c
@@ -1,31 +1,5 @@
 static int typhoon_ioctl(struct video_device *dev, unsigned int cmd, void *arg)
 {
   struct typhoon_device *typhoon = dev->priv;
-  
-  if (cmd == VIDIOCGTUNER) {
-    struct video_tuner v;
-    if (copy_from_user(v, arg, size_of(v)) != 0)
-      return -EFAULT;
-    if (v.tuner)	/* Only 1 tuner */
-      return -EINVAL;
-    v.rangelow = 875 * 1600;
-    v.rangehigh = 1080 * 1600;
-    v.flags = VIDEO_TUNER_LOW;
-    v.mode = VIDEO_MODE_AUTO;
-    v.signal = 0xFFFF;	/* We can't get the signal strength */
-    strcpy(v.name, "FM");
-    if (copy_to_user(arg, v, size_of(v)))
-      return -EFAULT;
-  }
-  else {
-    if (cmd == VIDIOCSTUNER) {
-      struct video_tuner v;
-      if (copy_from_user(v, arg, size_of(v)))
-	return -EFAULT; else {}
-      if (v.tuner != 0)
-	return -EINVAL;
-      /* Only 1 tuner so no setting needed ! */
-    } else { return -ENOIOCTLCMD; }
-  }
   return 0;  
 }
--- tests/video1.cocci
+++ tests/video1.cocci
@@ -1,26 +1,11 @@
 @@
 local function ioctlfn;
 identifier dev, cmd, arg;
-//fresh identifier i, f;
-identifier v;
-type T;
-statement S1, S2;
-identifier fld;
-expression E;
-statement S;
 @@
 
-//  ioctlfn(
-//        struct video_device *dev,
-//        unsigned int cmd, void *arg) {
-//    <...
-    if (cmd == E) {
-      ...
--     if (copy_from_user(v,arg,size_of(v)) != 0) S1
-      ...
--     if (copy_to_user(arg,v,size_of(v))) S2
-      ...
-    }
-    else { ... }
-//    ...>
-//  }
+  ioctlfn(
+-       struct video_device *dev,
++       struct inode *i, struct file *f,
+        unsigned int cmd, void *arg) {
+    ...
+  }
