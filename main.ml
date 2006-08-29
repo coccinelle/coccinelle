d5acbe39471e9a1f590f6d9d70eb60b7058cc3bd
Yoann Padioleau <pad@cs.uiuc.edu>
2006-08-29 12:44:36 +0000

    fix
    svn path=/coccinelle/; revision=947

--- main.ml
+++ main.ml
@@ -193,7 +193,7 @@ let main () =
       "-verbose_engine",       Arg.Set Flag_engine.debug_engine,    " ";
       "-loop",                 Arg.Set Flag_ctl.loop_in_src_code,    " ";
 
-      "-label_strategy_2",     Arg.Set Flag_parsing_c.label_strategy_2, " ";
+      "-l",     Arg.Set Flag_parsing_c.label_strategy_2, " ";
 
     ] in 
     let usage_msg = ("Usage: " ^ basename Sys.argv.(0) ^ 
