cb36275ce8bcfe3ccbdf06aa39e4f6b1295e544e
Yoann Padioleau <pad@cs.uiuc.edu>
2006-09-04 16:04:07 +0000

    fix
    svn path=/coccinelle/; revision=982

--- tests/nest3.c
+++ tests/nest3.c
@@ -3,14 +3,7 @@ void main(int i) {
   int i;
   char j;//int j;
 
-  //  g(1);
   for (i = 1; i < XXX; i++)
     f(i);
-  for (i = 1; i < XXX; i++)
-    f(i);
-//  g(1);
-//  for (i = 1; i < XXX; i++)
-//    f(i);
-  //g(i);
 
 }
