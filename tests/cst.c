864e866eff18f761b42c782f694f8549b7482ed6
Julia Lawall <julia.lawall@lip6.fr>
2006-11-30 10:47:33 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=1554

---
+++ tests/cst.c
@@ -0,0 +1,5 @@
+int main(int x) {
+  emu10k1_t *emu = snd_magic_cast(1, 2, return -ENXIO);
+  int z = 12;
+  return y;
+}
---
+++ tests/cst.cocci
@@ -0,0 +1,5 @@
+@@
+expression E1, E2, E3;
+@@
+- snd_magic_cast(E1, E2, E3)
++ 4
