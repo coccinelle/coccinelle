dc142e584c7850d8e575ada850be5075b0f86e11
Julia Lawall <julia.lawall@lip6.fr>
2007-01-09 12:34:47 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=1732

--- tools/gitgrep.ml
+++ tools/gitgrep.ml
@@ -1,3 +1,7 @@
+(* adjust as convenient *)
+let prefix = "/tmp/"
+let prefix = ""
+
 type dir = Minus | Plus
 
 let space = Str.regexp " "
@@ -22,16 +26,37 @@ let scan pattern dir i =
     | _ -> loop skipping git in
   loop false ""
 
+let dot = Str.regexp "\\."
+
+let open_git file =
+  let tmp = prefix^file in
+  if Sys.file_exists tmp
+  then open_in tmp
+  else
+    match List.rev (Str.split dot file) with
+      last::rest ->
+	let last_int = int_of_string last in
+	if last_int = 0
+	then
+	  failwith
+	    "can't go back one version from 0; make the log file by hand";
+	let prev =
+	  String.concat "." (List.rev ((string_of_int (last_int-1))::rest)) in
+	let _ =
+	  Sys.command
+	    (Printf.sprintf "git log -p v%s..v%s > %s" prev file tmp) in
+	open_in tmp
+    | _ -> open_in file
 
 let _ =
   if not(Array.length Sys.argv = 4)
-  then failwith "arguments: -/+ text file";
+  then failwith "arguments: -/+ text version";
   let dir =
     match Array.get Sys.argv 1 with
       "-" -> Minus
     | "+" -> Plus
     | _ -> failwith "bad direction" in
   let pattern = Str.regexp (Array.get Sys.argv 2) in
-  let i = open_in (Array.get Sys.argv 3) in
+  let i = open_git (Array.get Sys.argv 3) in
   try scan pattern dir i
   with End_of_file -> close_in i
