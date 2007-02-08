1037b3a18ea99503ecd4330a6c618c6801ea7c20
Julia Lawall <julia.lawall@lip6.fr>
2007-02-08 18:10:26 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=2040

---
+++ tests/empty.c
@@ -0,0 +1,9 @@
+static int vlsi_hard_start_xmit(struct sk_buff *skb, struct net_device *ndev)
+{
+		pci_restore_state(pdev, idev->cfg_space);
+		if (ring_first(idev->tx_ring) == NULL) {
+		}
+		else
+			;
+
+}
