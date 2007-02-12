dd10705ac3cc31d8c7c21f9d894bc4034db56256
Julia Lawall <julia.lawall@lip6.fr>
2007-02-12 13:05:29 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=2096

---
+++ tests/fnptr.c
@@ -0,0 +1,12 @@
+static irqreturn_t sci_tx_interrupt(int irq, void *ptr, struct pt_regs *regs)
+{
+}
+
+static int sci_request_irq(struct sci_port *port) {
+  int i;
+  irqreturn_t (*handlers[4])(int irq, void *ptr, struct pt_regs *regs) = {
+    sci_er_interrupt, sci_rx_interrupt, sci_tx_interrupt,
+    sci_br_interrupt,
+  };
+  request_irq(port->irqs[0], sci_mpxed_interrupt, SA_INTERRUPT);
+}
