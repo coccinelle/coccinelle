0da7f46e28ce535defc8ab008fc7dc69c3fab87d
Julia Lawall <julia.lawall@lip6.fr>
2007-02-13 10:58:48 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=2104

---
+++ tests/tadb.c
@@ -0,0 +1,15 @@
+struct adbhid {
+	struct input_dev input;
+};
+
+static struct adbhid *adbhid[16] = { 0 };
+
+
+static void
+adbhid_keyboard_input()
+{
+  int id;
+	if (!adbhid[id]) {
+		return;
+	}
+}
