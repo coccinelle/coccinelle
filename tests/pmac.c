480ab57340d16f3f317c0c0652f4779632a0779c
Julia Lawall <julia.lawall@lip6.fr>
2007-02-26 15:03:05 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=2205

---
+++ tests/pmac.c
@@ -0,0 +1,7 @@
+#ifdef CONFIG_PPC_HAS_FEATURE_CALLS
+#include <asm/pmac_feature.h>
+#else
+#include <asm/feature.h>
+#endif
+
+#define chip_t pmac_t
---
+++ tests/pmac.res
@@ -0,0 +1,5 @@
+#ifdef CONFIG_PPC_HAS_FEATURE_CALLS
+#include <asm/pmac_feature.h>
+#else
+#include <asm/feature.h>
+#endif
