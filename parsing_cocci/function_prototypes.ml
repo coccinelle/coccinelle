68e6707ab115f591d6bafffccb3d807d59b7c959
Julia Lawall <julia.lawall@lip6.fr>
2007-03-05 10:01:47 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=2285

---
+++ parsing_cocci/function_prototypes.ml
@@ -0,0 +1,219 @@
+module Ast0 = Ast0_cocci
+module Ast = Ast_cocci
+module V0 = Visitor_ast0
+
+let rec get_name name =
+  match Ast0.unwrap name with
+    Ast0.Id(nm) | Ast0.MetaId(nm,_) | Ast0.MetaFunc(nm,_)
+  | Ast0.MetaLocalFunc(nm,_) -> Ast0.unwrap_mcode nm
+  | Ast0.OptIdent(id) | Ast0.UniqueIdent(id) | Ast0.MultiIdent(id) ->
+      get_name id
+
+(* --------------------------------------------------------------------- *)
+(* collect all of the functions *)
+
+let brace_to_semi (_,arity,info,mcodekind) = (";",Ast0.NONE,info,mcodekind)
+
+let collect_function (stm : Ast0.statement) =
+  match Ast0.unwrap stm with
+    Ast0.FunDecl(_,stg,ty,name,lp,params,rp,lbrace,body,rbrace) ->
+      [(get_name name,
+	Ast0.rewrap stm
+	  (Ast0.Decl((Ast0.default_info(),Ast0.context_befaft()),
+		     Ast0.rewrap stm
+		       (Ast0.UnInit
+			  (stg,
+			   Ast0.rewrap stm
+			     (Ast0.FunctionType(ty,lp,params,rp)),
+			   name,brace_to_semi lbrace)))))]
+  | _ -> []
+	
+let collect_functions stmt_dots =
+  List.concat (List.map collect_function (Ast0.undots stmt_dots))
+    
+let get_all_functions minus rule =
+  let res =
+    match Ast0.unwrap rule with
+      Ast0.DECL(stmt) -> collect_function stmt
+    | Ast0.CODE(rule_elem_dots) -> collect_functions rule_elem_dots
+    | _ -> [] in
+  if minus
+  then
+    List.map
+      (function (nm,vl) ->
+	(nm,(Iso_pattern.rebuild_mcode None).V0.rebuilder_statement vl))
+      res
+  else res
+
+(* --------------------------------------------------------------------- *)
+(* try to match up the functions *)
+
+(* pass through the - and + functions in lockstep, until one runs out.
+Then process the remaining minuses, if any.  If we can find another
+function of the same name for either the current - or + function, take that
+one.  Otherwise, align the two current ones. *)
+
+let rec align all_minus all_plus =
+  let rec loop = function
+      ([],_) -> []
+    | ((mname,mproto)::minus,[]) ->
+	(try
+	  let pproto = List.assoc mname all_plus in
+	  (mname,mproto,Some pproto)::(loop (minus,[]))
+	with Not_found -> (mname,mproto,None)::(loop (minus, [])))
+    | ((mname,mproto)::minus,(pname,pproto)::plus) ->
+	if mname = pname
+	then (mname,mproto,Some pproto)::(loop (minus, []))
+	else
+	  (try
+	    let pproto_for_minus = List.assoc mname all_plus in
+	    (try
+	      let _ = List.assoc mname all_plus in
+	      (* protos that match both *)
+	      (mname,mproto,Some pproto_for_minus)::(loop (minus, plus))
+	    with Not_found ->
+	      (* proto that matches only minus *)
+	      (mname,mproto,Some pproto_for_minus)::
+	      (loop (minus, ((pname,pproto)::plus))))
+	  with Not_found ->
+	    (try
+	      let _ = List.assoc mname all_plus in
+	      (* proto only for plus *)
+	      (mname,mproto,None)::(loop (minus, plus))
+	    with Not_found ->
+	      (* protos for no one *)
+	      (mname,mproto,Some pproto)::(loop (minus, plus)))) in
+  List.filter changed_proto (loop (all_minus, all_plus))
+
+(* --------------------------------------------------------------------- *)
+
+and strip =
+  let donothing r k e =
+    let (x,_,_,_,_,_) = k e in
+    (x,Ast0.default_info(),ref 0,ref Ast0.PLUS,ref None,Ast0.NoDots) in
+  let mcode (mc,_,_,_) = (mc,Ast0.NONE,Ast0.default_info(),Ast0.PLUS) in
+
+  (* need a case for everything that has an unvisited component and can be in
+     a function prototype *)
+
+  let ident r k e =
+    donothing r k
+      (Ast0.rewrap e
+	 (match Ast0.unwrap e with
+	   Ast0.MetaId(nm,pure) -> Ast0.MetaId(nm,true)
+	 | Ast0.MetaFunc(nm,pure) -> Ast0.MetaFunc(nm,true)
+	 | Ast0.MetaLocalFunc(nm,pure) -> Ast0.MetaLocalFunc(nm,true)
+	 | e -> e)) in
+
+  let typeC r k e =
+    donothing r k
+      (Ast0.rewrap e
+	 (match Ast0.unwrap e with
+	   Ast0.MetaType(nm,pure) -> Ast0.MetaType(nm,true)
+	 | e -> e)) in
+
+  let param r k e =
+    donothing r k
+      (Ast0.rewrap e
+	 (match Ast0.unwrap e with
+	   Ast0.MetaParam(nm,pure) -> Ast0.MetaParam(nm,true)
+	 | Ast0.MetaParamList(nm,pure) -> Ast0.MetaParamList(nm,true)
+	 | e -> e)) in
+
+  V0.rebuilder
+    mcode mcode mcode mcode mcode mcode mcode mcode mcode mcode mcode
+    donothing donothing donothing donothing donothing
+    ident donothing typeC donothing param donothing donothing
+    donothing donothing
+
+and changed_proto = function
+    (mname,mproto,None) -> true
+  | (mname,mproto,Some pproto) ->
+      not ((strip.V0.rebuilder_statement mproto) =
+	   (strip.V0.rebuilder_statement pproto))
+
+(* --------------------------------------------------------------------- *)
+(* make rules *)
+
+let rec drop_param_name p =
+  Ast0.rewrap p
+    (match Ast0.unwrap p with
+      Ast0.Param(p,_) -> Ast0.Param(p,None)
+    | Ast0.OptParam(p) -> Ast0.OptParam(drop_param_name p)
+    | Ast0.UniqueParam(p) -> Ast0.UniqueParam(p)
+    | p -> p)
+
+let drop_names minus dec =
+  let dec =
+    if minus
+    then (Iso_pattern.rebuild_mcode None).V0.rebuilder_statement dec
+    else dec in
+  match Ast0.unwrap dec with
+    Ast0.Decl(info,uninit) ->
+      (match Ast0.unwrap uninit with
+	Ast0.UnInit(stg,typ,name,sem) ->
+	  (match Ast0.unwrap typ with
+	    Ast0.FunctionType(ty,lp,params,rp) ->
+	      let params =
+		match Ast0.unwrap params with
+		  Ast0.DOTS(l) ->
+		    Ast0.rewrap params (Ast0.DOTS(List.map drop_param_name l))
+		| Ast0.CIRCLES(l) ->
+		    Ast0.rewrap params
+		      (Ast0.CIRCLES(List.map drop_param_name l))
+		| Ast0.STARS(l) -> failwith "unexpected stars" in
+	      Ast0.rewrap dec
+		(Ast0.Decl
+		   (info,
+		    Ast0.rewrap uninit
+		      (Ast0.UnInit
+			 (stg,
+			  Ast0.rewrap typ
+			    (Ast0.FunctionType(ty,lp,params,rp)),
+			  name,sem))))
+	  | _ -> failwith "unexpected type")
+      |	_ -> failwith "unexpected declaration")
+  | _ -> failwith "unexpected term"
+
+let merge mproto pproto =
+  let mproto = [Ast0.rewrap mproto (Ast0.DECL mproto)] in
+  let pproto = [Ast0.rewrap pproto (Ast0.DECL pproto)] in
+  Insert_plus.insert_plus mproto pproto
+
+let make_rule = function
+    (mname,mproto,Some pproto) ->
+      let no_name_mproto = drop_names true mproto in
+      let no_name_pproto = drop_names false pproto in
+      merge mproto pproto;
+      merge no_name_mproto no_name_pproto;
+      [mproto;no_name_mproto]
+  | (mname,mproto,None) -> [mproto;drop_names true mproto]
+
+let make_rules minus plus =
+  let minus_functions =
+    List.concat (List.map (get_all_functions true) minus) in
+  match minus_functions with
+    [] -> None
+  | _ ->
+      let plus_functions =
+	List.concat (List.map (get_all_functions false) plus) in
+      let protos = align minus_functions plus_functions in
+      let rules =
+	List.map Ast0toast.statement
+	  (List.concat (List.map make_rule protos)) in
+      match rules with
+	[] -> None
+      | [x] ->
+	  (* probably not possible, since there is always the version with
+	     variables and the version without *)
+	  Some [Ast.rewrap x (Ast.DECL x)]
+      |	x::_ ->
+	  let drules =
+	    List.map (function x -> Ast.rewrap x (Ast.DOTS [x])) rules in
+	  Some [Ast.rewrap x (Ast.DECL (Ast.rewrap x (Ast.Disj drules)))]
+
+(* --------------------------------------------------------------------- *)
+(* entry point *)
+
+let process minus plus = make_rules minus plus
+
