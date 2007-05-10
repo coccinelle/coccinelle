168fba98a2051d061909a3853ae98ad5dee643fb
Julia Lawall <julia.lawall@lip6.fr>
2007-05-10 16:05:40 +0000

    to reduce the size of parse_cocci_menhir.mly
    svn path=/coccinelle/; revision=2750

---
+++ parsing_cocci/parse_aux.ml
@@ -0,0 +1,249 @@
+(* exports everything, used only by parser_cocci_menhir.mly *)
+module Ast0 = Ast0_cocci
+module Ast = Ast_cocci
+
+let make_info line logical_line offset col strbef straft =
+  { Ast0.line_start = line; Ast0.line_end = line;
+    Ast0.logical_start = logical_line; Ast0.logical_end = logical_line;
+    Ast0.attachable_start = true; Ast0.attachable_end = true;
+    Ast0.mcode_start = []; Ast0.mcode_end = [];
+    Ast0.column = col; Ast0.offset = offset;
+    Ast0.strings_before = strbef; Ast0.strings_after = straft; }
+
+let clt2info (_,line,logical_line,offset,col,strbef,straft) =
+  make_info line logical_line offset col strbef straft
+
+let drop_bef (arity,line,lline,offset,col,strbef,straft) =
+  (arity,line,lline,offset,col,[],straft)
+
+let drop_aft (arity,line,lline,offset,col,strbef,straft) =
+  (arity,line,lline,offset,col,strbef,[])
+
+let clt2mcode str = function
+    (Data.MINUS,line,lline,offset,col,strbef,straft)       ->
+      (str,Ast0.NONE,make_info line lline offset col strbef straft,
+       Ast0.MINUS(ref([],Ast0.default_token_info)))
+  | (Data.OPTMINUS,line,lline,offset,col,strbef,straft)    ->
+      (str,Ast0.OPT,make_info line lline offset col strbef straft,
+       Ast0.MINUS(ref([],Ast0.default_token_info)))
+  | (Data.UNIQUEMINUS,line,lline,offset,col,strbef,straft) ->
+      (str,Ast0.UNIQUE,make_info line lline offset col strbef straft,
+       Ast0.MINUS(ref([],Ast0.default_token_info)))
+  | (Data.MULTIMINUS,line,lline,offset,col,strbef,straft) ->
+      (str,Ast0.MULTI,make_info line lline offset col strbef straft,
+       Ast0.MINUS(ref([],Ast0.default_token_info)))
+  | (Data.PLUS,line,lline,offset,col,strbef,straft)        ->
+      (str,Ast0.NONE,make_info line lline offset col strbef straft,Ast0.PLUS)
+  | (Data.CONTEXT,line,lline,offset,col,strbef,straft)     ->
+      (str,Ast0.NONE,make_info line lline offset col strbef straft,
+       Ast0.CONTEXT(ref(Ast.NOTHING,
+			Ast0.default_token_info,Ast0.default_token_info)))
+  | (Data.OPT,line,lline,offset,col,strbef,straft)         ->
+      (str,Ast0.OPT,make_info line lline offset col strbef straft,
+       Ast0.CONTEXT(ref(Ast.NOTHING,
+			Ast0.default_token_info,Ast0.default_token_info)))
+  | (Data.UNIQUE,line,lline,offset,col,strbef,straft)      ->
+      (str,Ast0.UNIQUE,make_info line lline offset col strbef straft,
+       Ast0.CONTEXT(ref(Ast.NOTHING,
+			Ast0.default_token_info,Ast0.default_token_info)))
+  | (Data.MULTI,line,lline,offset,col,strbef,straft)      ->
+      (str,Ast0.MULTI,make_info line lline offset col strbef straft,
+       Ast0.CONTEXT(ref(Ast.NOTHING,
+			Ast0.default_token_info,Ast0.default_token_info)))
+
+let id2name   (name, clt) = name
+let id2clt    (name, clt) = clt
+let id2mcode  (name, clt) = clt2mcode name clt
+
+let mkdots str (dot,whencode) =
+  match str with
+    "..." -> Ast0.wrap(Ast0.Dots(clt2mcode str dot, whencode))
+  | "ooo" -> Ast0.wrap(Ast0.Circles(clt2mcode str dot, whencode))
+  | "***" -> Ast0.wrap(Ast0.Stars(clt2mcode str dot, whencode))
+  | _ -> failwith "cannot happen"
+
+let mkedots str (dot,whencode) =
+  match str with
+    "..." -> Ast0.wrap(Ast0.Edots(clt2mcode str dot, whencode))
+  | "ooo" -> Ast0.wrap(Ast0.Ecircles(clt2mcode str dot, whencode))
+  | "***" -> Ast0.wrap(Ast0.Estars(clt2mcode str dot, whencode))
+  | _ -> failwith "cannot happen"
+
+let mkidots str (dot,whencode) =
+  match str with
+    "..." -> Ast0.wrap(Ast0.Idots(clt2mcode str dot, whencode))
+  | _ -> failwith "cannot happen"
+
+let mkddots str (dot,whencode) =
+  match str with
+    "..." -> Ast0.wrap(Ast0.Ddots(clt2mcode str dot, whencode))
+  | _ -> failwith "cannot happen"
+
+let mkpdots str dot =
+  match str with
+    "..." -> Ast0.wrap(Ast0.Pdots(clt2mcode str dot))
+  | "ooo" -> Ast0.wrap(Ast0.Pcircles(clt2mcode str dot))
+  | _ -> failwith "cannot happen"
+
+let arith_op ast_op left op right =
+  Ast0.wrap
+    (Ast0.Binary(left, clt2mcode (Ast.Arith ast_op) op, right))
+
+let logic_op ast_op left op right =
+  Ast0.wrap
+    (Ast0.Binary(left, clt2mcode (Ast.Logical ast_op) op, right))
+
+let make_cv cv ty =
+  match cv with None -> ty | Some x -> Ast0.wrap (Ast0.ConstVol(x,ty))
+
+let top_dots l =
+  let circle x =
+    match Ast0.unwrap x with Ast0.Circles(_) -> true | _ -> false in
+  let star x =
+    match Ast0.unwrap x with Ast0.Stars(_) -> true | _ -> false in
+  if List.exists circle l
+  then Ast0.wrap(Ast0.CIRCLES(l))
+  else
+    if List.exists star l
+    then Ast0.wrap(Ast0.STARS(l))
+    else Ast0.wrap(Ast0.DOTS(l))
+
+(* here the offset is that of the first in the sequence of *s, not that of
+each * individually *)
+let pointerify ty m =
+  List.fold_left
+    (function inner ->
+      function cur ->
+	Ast0.wrap(Ast0.Pointer(inner,clt2mcode "*" cur)))
+    ty m
+
+let ty_pointerify ty m =
+  List.fold_left
+    (function inner -> function cur -> Type_cocci.Pointer(inner))
+    ty m
+
+(* Left is <=>, Right is =>.  Collect <=>s. *)
+let iso_adjust fn first rest =
+  let rec loop = function
+      [] -> [[]]
+    | (Common.Left x)::rest ->
+	(match loop rest with
+	  front::after -> (fn x::front)::after
+	| _ -> failwith "not possible")
+    | (Common.Right x)::rest ->
+	(match loop rest with
+	  front::after -> []::(fn x::front)::after
+	| _ -> failwith "not possible") in
+  match loop rest with
+    front::after -> (fn first::front)::after
+  | _ -> failwith "not possible"
+
+let check_meta tok =
+  let lookup rule name =
+    try
+      let info = Hashtbl.find Data.all_metadecls rule in
+      List.find (function mv -> Ast.get_meta_name mv = (rule,name)) info
+    with
+      Not_found ->
+	raise
+	  (Semantic_cocci.Semantic
+	     ("bad rule "^rule^" or bad variable "^name)) in
+  match tok with
+    Ast.MetaIdDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaIdDecl(_,_) | Ast.MetaFreshIdDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaFreshIdDecl(Ast.NONE,(rule,name)) ->
+      raise
+	(Semantic_cocci.Semantic
+	   "can't inherit the freshness of an identifier")
+  | Ast.MetaTypeDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaTypeDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaParamDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaParamDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaParamListDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaParamListDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaErrDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaErrDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaExpDecl(Ast.NONE,(rule,name),ty) ->
+      (match lookup rule name with
+	Ast.MetaExpDecl(_,_,ty1) when ty = ty1 -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaExpListDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaExpListDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaStmDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaStmDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaStmListDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaStmListDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaFuncDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaFuncDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaLocalFuncDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaLocalFuncDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaConstDecl(Ast.NONE,(rule,name),ty) ->
+      (match lookup rule name with
+	Ast.MetaConstDecl(_,_,ty1) when ty = ty1 -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | Ast.MetaTextDecl(Ast.NONE,(rule,name)) ->
+      (match lookup rule name with
+	Ast.MetaTextDecl(_,_) -> ()
+      | _ ->
+	  raise
+	    (Semantic_cocci.Semantic
+	       ("incompatible inheritance declaration "^name)))
+  | _ ->
+      raise
+	(Semantic_cocci.Semantic ("arity not allowed on imported declaration"))
