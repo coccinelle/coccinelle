3916cea5f3013bb0f4d6c2b89ab2de954df3dc80
Julia Lawall <julia.lawall@lip6.fr>
2007-05-18 14:40:22 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=2767

--- popl/popltoctl.ml
+++ popl/popltoctl.ml
@@ -45,7 +45,7 @@ let exit           = wrap 0 (CTL.Pred(Lib_engine.Exit, CTL.Control))
 
 let ctl_au x y     = wrap 0 (CTL.AU(CTL.FORWARD,CTL.STRICT,x,ctl_or y after))
 
-let ctl_exists v x = wrap 0 (CTL.Exists(v,x,true))
+let ctl_exists v x keep_wit = wrap 0 (CTL.Exists(v,x,keep_wit))
 
 let ctl_uncheck x  = wrap 0 (CTL.Uncheck(x))
 
@@ -62,7 +62,7 @@ let contains_modif =
     let res = k re in
     match Ast.unwrap re with
       Ast.FunHeader(bef,_,fninfo,name,lp,params,rp) ->
-	bind (mcode r ((),(),bef)) res
+      bind (mcode r ((),(),bef)) res
     | Ast.Decl(bef,_,decl) -> bind (mcode r ((),(),bef)) res
     | _ -> res in
   let recursor =
@@ -74,65 +74,69 @@ let contains_modif =
   recursor.V.combiner_rule_elem
 
 let predmaker guard term =
-  if not guard && contains_modif term
+  if guard && contains_modif term
   then
     let v = ("","_v") in
-    ctl_exists v (wrap 0 (CTL.Pred (Lib_engine.Match(term),CTL.Modif v)))
+    ctl_exists v (wrap 0 (CTL.Pred (Lib_engine.Match(term),CTL.Modif v))) true
   else wrap 0 (CTL.Pred (Lib_engine.Match(term),CTL.Control))
 
 (* --------------------------------------------------------------------- *)
 
-let rec ctl_seq unchecked a = function
+let rec ctl_seq keep_wit a = function
     Past.Seq(elem,seq) ->
-      ctl_element unchecked (ctl_seq unchecked a seq) elem
+      ctl_element keep_wit (ctl_seq keep_wit a seq) elem
   | Past.Empty -> a
-  | Past.SExists(var,seq) -> ctl_exists var (ctl_seq unchecked a seq)
+  | Past.SExists(var,seq) -> ctl_exists var (ctl_seq keep_wit a seq) keep_wit
 
-and ctl_element unchecked a = function
-    Past.Term(term) -> ctl_and (predmaker unchecked term) (ctl_ax a)
+and ctl_element keep_wit a = function
+    Past.Term(term) -> ctl_and (predmaker keep_wit term) (ctl_ax a)
   | Past.Or(seq1,seq2) ->
-      ctl_seqor (ctl_seq unchecked a seq1) (ctl_seq unchecked a seq2)
+      ctl_seqor (ctl_seq keep_wit a seq1) (ctl_seq keep_wit a seq2)
   | Past.DInfo(dots,seq_bef,seq_aft) ->
       let shortest =
 	List.fold_left ctl_or ctl_false
-	  (List.map (ctl_element true ctl_true)
+	  (List.map (ctl_element false ctl_true)
 	     (Common.union_set seq_bef seq_aft)) in
-      ctl_au (ctl_and (guard_ctl_dots unchecked dots) (ctl_not shortest)) a
-  | Past.EExists(var,elem) -> ctl_exists var (ctl_element unchecked a elem)
+      ctl_au (ctl_and (guard_ctl_dots keep_wit dots) (ctl_not shortest)) a
+  | Past.EExists(var,elem) ->
+      ctl_exists var (ctl_element keep_wit a elem) keep_wit
 
 (* --------------------------------------------------------------------- *)
 
-and guard_ctl_seq unchecked = function
-    Past.Seq(elem,Past.Empty) -> guard_ctl_element unchecked elem
+and guard_ctl_seq keep_wit = function
+    Past.Seq(elem,Past.Empty) -> guard_ctl_element keep_wit elem
   | Past.Seq(elem,seq) ->
-      ctl_element unchecked (guard_ctl_seq unchecked seq) elem
+      ctl_element keep_wit (guard_ctl_seq keep_wit seq) elem
   | Past.Empty -> ctl_true
-  | Past.SExists(var,seq) -> ctl_exists var (guard_ctl_seq unchecked seq)
+  | Past.SExists(var,seq) ->
+      ctl_exists var (guard_ctl_seq keep_wit seq) keep_wit
 
-and guard_ctl_element unchecked = function
-    Past.Term(term) -> predmaker unchecked term
+and guard_ctl_element keep_wit = function
+    Past.Term(term) -> predmaker keep_wit term
   | Past.Or(seq1,seq2) ->
-      ctl_seqor (guard_ctl_seq unchecked seq1) (guard_ctl_seq unchecked seq2)
+      ctl_seqor (guard_ctl_seq keep_wit seq1) (guard_ctl_seq keep_wit seq2)
   | Past.DInfo(dots,seq_bef,seq_aft) ->
       let shortest =
 	List.fold_left ctl_or ctl_false
-	  (List.map (ctl_element true ctl_true)
+	  (List.map (ctl_element false ctl_true)
 	     (Common.union_set seq_bef seq_aft)) in
       let aft = ctl_or shortest exit in
-      ctl_au (ctl_and (guard_ctl_dots unchecked dots) (ctl_not shortest)) aft
-  | Past.EExists(var,elem) -> ctl_exists var (guard_ctl_element unchecked elem)
+      ctl_au (ctl_and (guard_ctl_dots keep_wit dots) (ctl_not shortest)) aft
+  | Past.EExists(var,elem) ->
+      ctl_exists var (guard_ctl_element keep_wit elem) keep_wit
 
-and guard_ctl_dots unchecked = function
+and guard_ctl_dots keep_wit = function
     Past.Dots -> ctl_true
-  | Past.Nest(_) when unchecked -> ctl_true
+  | Past.Nest(_) when not keep_wit -> ctl_true
   | Past.Nest(seq) ->
-      ctl_or (guard_ctl_seq false seq) (ctl_not (guard_ctl_seq true seq))
+      ctl_or (guard_ctl_seq true seq) (ctl_not (guard_ctl_seq false seq))
   | Past.When(dots,seq) ->
       ctl_and
-	(guard_ctl_dots unchecked dots)
-	(ctl_not (ctl_seq true ctl_true seq))
-  | Past.DExists(var,dots) -> ctl_exists var (guard_ctl_dots unchecked dots)
+	(guard_ctl_dots keep_wit dots)
+	(ctl_not (ctl_seq false ctl_true seq))
+  | Past.DExists(var,dots) ->
+      ctl_exists var (guard_ctl_dots keep_wit dots) keep_wit
 
 (* --------------------------------------------------------------------- *)
 
-let toctl sl = ctl_seq false ctl_true sl
+let toctl sl = ctl_seq true ctl_true sl
