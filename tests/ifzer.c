7b96bdd0248fe1954c7ae13b8872db857c5e15f3
Julia Lawall <julia.lawall@lip6.fr>
2007-06-03 16:42:18 +0000

    problem with quote in comment - interpreted as the start of a character
    svn path=/coccinelle/; revision=2805

---
+++ tests/ifzer.c
@@ -0,0 +1,8 @@
+int main() {
+#if 0 /* Accessing floppy->pc is not valid here, the previous pc may be gone
+          and have lived on another thread's stack; that stack may have become
+          unmapped meanwhile (CONFIG_DEBUG_PAGEALLOC). */
+#endif
+ foo();
+}
+ 
