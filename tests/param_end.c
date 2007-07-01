964e636a0348af233db787447b30d1bb3b2e02d4
Julia Lawall <julia.lawall@lip6.fr>
2007-07-01 13:10:44 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=2901

---
+++ tests/param_end.c
@@ -0,0 +1,15 @@
+int one (int x) { return; }
+
+int two (int a, int x, int b) { return; }
+
+int three (int x, int a) { return; }
+
+int four (int a, int x) { return; }
+
+int yone (int y) { return; }
+
+int ytwo (int a, int y, int b) { return; }
+
+int ythree (int y, int a) { return; }
+
+int yfour (int a, int y) { return; }
---
+++ tests/param_end.res
@@ -0,0 +1,15 @@
+int one () { return; }
+
+int two (int a, int b) { return; }
+
+int three (int a) { return; }
+
+int four (int a) { return; }
+
+int yone () { return; }
+
+int ytwo (int a, int b) { return; }
+
+int ythree (int a) { return; }
+
+int yfour (int a) { return; }
