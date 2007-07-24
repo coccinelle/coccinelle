2770aea53853aa3d9524b88484bad2012127f9f7
Yoann Padioleau <pad@cs.uiuc.edu>
2007-07-24 13:06:19 +0000

    added file
    svn path=/coccinelle/; revision=3024

---
+++ flag_cocci.ml
@@ -0,0 +1,31 @@
+(* the inputs *)
+let show_c = ref false
+let show_cocci = ref false
+
+(* the output *)
+let show_diff = ref true
+
+(* the derived inputs *)
+let show_flow = ref false
+let show_before_fixed_flow = ref false
+
+let show_ctl_tex = ref false
+let show_ctl_text = ref true
+
+let inline_let_ctl = ref false
+let show_mcodekind_in_ctl = ref false
+
+(* the "underived" outputs *)
+let show_transinfo = ref true
+let show_binding_in_out = ref false
+
+
+
+
+let windows = ref false
+
+let popl = ref false
+
+let include_path = ref "include"
+
+let timeout = ref (None : int option)
