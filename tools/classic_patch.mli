af3d36d98fa75ff629cab40746381fa29693ed9c
Yoann Padioleau <pad@cs.uiuc.edu>
2007-07-25 08:01:30 +0000

    new tool
    svn path=/coccinelle/; revision=3028

---
+++ tools/classic_patch.mli
@@ -0,0 +1,7 @@
+open Common
+
+type patch = patchitem list
+ and patchitem = File of filename * string (* header line *) * string list
+
+val parse_patch : filename -> patch
+val unparse_patch : patch -> filename (* outfile *) -> unit
---
+++ tools/split_patch.ml
@@ -0,0 +1,114 @@
+open Common open Commonop
+
+module CP = Classic_patch 
+
+(* ./split_patch ../demos/janitorings/patch-kzalloc-vnew3.patch /tmp/xx "0 -> NULL" ../bodymail.doc  *)
+
+(*****************************************************************************)
+(* Helpers *)
+(*****************************************************************************)
+
+let print_header_patch patch = 
+  patch +> List.iter (function CP.File (s, header, body) -> pr s)
+
+let group_patch patch = 
+  let patch' = patch +> List.map (function (CP.File (s, header, body)) -> 
+    Common.split "/" (Common.dirname s), 
+    (CP.File (s, header, body))
+  )
+  in
+  let rec aux_patch xs = 
+    match xs with
+    | [] -> []
+    | (dir_elems,x)::xs -> 
+        if List.length dir_elems >= 2 then
+          let base = Common.take 2 dir_elems in
+          let (yes, no) = xs +> Common.partition_either (fun (dir_elems', x) ->
+            if List.length dir_elems' >= 2 && 
+               Common.take 2 dir_elems' = base
+            then Left x
+            else Right (dir_elems', x)
+          ) in
+          (Common.join "/" base, (x::yes))::aux_patch no
+        else 
+          (Common.join "/" dir_elems, [x])::aux_patch xs
+  in
+  aux_patch patch'
+
+let test_patch file = 
+  let patch = CP.parse_patch file in
+  let groups = group_patch patch in
+  groups +> List.iter (fun (dir, minipatch) -> 
+    print_header_patch minipatch;
+    pr ""
+  )
+
+let i_to_s_padded i = 
+  match i with
+  | i when i < 10 -> "0" ^ i_to_s i
+  | i when i < 100 -> i_to_s i
+  | i -> i_to_s i
+
+let split_patch str bodymail prefix file = 
+  let patch = CP.parse_patch file in
+  let minipatches = group_patch patch in
+  let total = List.length minipatches in
+  let minipatches_indexed = Common.index_list_1 minipatches in
+  minipatches_indexed +> List.iter (fun ((dir,minipatch), i) -> 
+    let numpatch = (i_to_s_padded  i) in
+    let tmpfile = prefix ^  numpatch ^ ".mail" in
+    let patchfile = "/tmp/x.patch" in
+    pr2 ("generating :" ^ tmpfile ^ " for " ^ dir);
+    CP.unparse_patch minipatch patchfile;
+    Common.with_open_outfile tmpfile (fun (pr_no_nl, chan) -> 
+      let pr s = pr_no_nl (s ^ "\n") in
+      pr "To: linux-kernel@vger.kernel.org";
+      pr (sprintf "Subject: [PATCH %s/%d] %s, for %s" 
+             numpatch total str dir);
+      pr "Cc: akpm@linux-foundation.org, linux-kernel@vger.kernel.org";
+      pr "";
+      cat bodymail +> List.iter pr;
+      pr "";
+      pr "Signed-off-by: Yoann Padioleau <padator@wanadoo.fr>";
+      pr "";
+    );
+
+    command2(sprintf "diffstat %s >> %s" patchfile tmpfile);
+    command2(sprintf "echo >> %s" tmpfile);
+    command2(sprintf "cat %s >> %s" patchfile tmpfile);
+  )
+  
+
+
+(*****************************************************************************)
+(* Flags *)
+(*****************************************************************************)
+
+(*****************************************************************************)
+(* main entry point *)
+(*****************************************************************************)
+
+let main () = 
+  begin
+    let args = ref [] in
+    let options = [
+    ] in
+    let usage_msg = 
+      "Usage: " ^ basename Sys.argv.(0) ^ 
+        " <file> [options]" ^ "\n" ^ "Options are:"
+    in
+
+    Arg.parse (Arg.align options) (fun x -> args := x::!args) usage_msg;
+    args := List.rev !args;
+
+    (match (!args) with
+    | [patch] -> test_patch patch
+    | [patch;prefix;str;bodymail] -> split_patch str bodymail prefix patch
+    | _ -> Arg.usage (Arg.align options) usage_msg; 
+    )
+  end
+
+(*****************************************************************************)
+let _ =
+  main ()
+
