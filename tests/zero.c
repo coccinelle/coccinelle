9b736ed7a5a0f12ff322810fc9d6b019173cc94a
Julia Lawall <julia.lawall@lip6.fr>
2007-08-06 19:36:08 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=3107

---
+++ tests/zero.c
@@ -0,0 +1,5 @@
+int main () {
+  memset(command, 0, sizeof(struct sbp2_command_info));
+  memset(command, 0x00, sizeof(struct sbp2_command_info));
+  memset(command, '\0', sizeof(struct sbp2_command_info));
+}
---
+++ tests/zero.cocci
@@ -0,0 +1,5 @@
+@@
+expression E1, E2;
+@@
+
+- memset(E1,0,E2);
