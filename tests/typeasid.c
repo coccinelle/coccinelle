5c8d0fecd74115c66477106029173fa67acb215d
Julia Lawall <julia.lawall@lip6.fr>
2007-09-05 20:37:43 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=3240

--- tests/typeasid.c
+++
@@ -1,8 +0,0 @@
-typedef struct snd_card_sa11xx_uda1341 {
-	snd_card_t *card;
-} sa11xx_uda1341_t;
-
-snd_kcontrol_t *snd_ctl_new(snd_kcontrol_t * control, unsigned int access)
-{
-	sa11xx_uda1341_t *chip = xxx(sa11x_uda1341_t, card->pm_private_data);
-}
