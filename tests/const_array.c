121852591a280089e6871d519351f520d5e432d8
Julia Lawall <julia.lawall@lip6.fr>
2007-09-09 07:11:58 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=3301

--- tests/const_array.c
+++ tests/const_array.c
@@ -13,8 +13,14 @@ static char *r128_family2[] = {
 	"PCI",
 };
 
+static struct foo *r128_family3[] = {
+	"AGP",
+	"PCI",
+};
+
 int main () {
   ent->driver_data = sizeof(r128_family)/sizeof(char *);
   ent->driver_data = sizeof(r128_family1)/sizeof(char *);
   ent->driver_data = sizeof(r128_family2)/sizeof(char *);
+  ent->driver_data = sizeof(r128_family3)/sizeof(struct foo *);
 }
--- tests/const_array.res
+++ tests/const_array.res
@@ -17,4 +17,5 @@ int main () {
   ent->driver_data = ARRAY_SIZE(r128_family);
   ent->driver_data = ARRAY_SIZE(r128_family1);
   ent->driver_data = ARRAY_SIZE(r128_family2);
+  ent->driver_data = ARRAY_SIZE(r128_family3);
 }
