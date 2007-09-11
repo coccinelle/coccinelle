43e3edcc71435e3140181de891136e999fc4c319
Yoann Padioleau <pad@cs.uiuc.edu>
2007-09-11 04:41:28 +0000

    test with const struct
    svn path=/coccinelle/; revision=3334

--- tests/const_array.c
+++ tests/const_array.c
@@ -18,9 +18,15 @@ static struct foo *r128_family3[] = {
 	"PCI",
 };
 
+static const struct foo *r128_family4[] = {
+	"AGP",
+	"PCI",
+};
+
 int main () {
   ent->driver_data = sizeof(r128_family)/sizeof(char *);
   ent->driver_data = sizeof(r128_family1)/sizeof(char *);
   ent->driver_data = sizeof(r128_family2)/sizeof(char *);
   ent->driver_data = sizeof(r128_family3)/sizeof(struct foo *);
+  ent->driver_data = sizeof(r128_family4)/sizeof(struct foo *);
 }
