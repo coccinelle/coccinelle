976c3af04f6716315183057ed15e290cfa05068c
Julia Lawall <julia.lawall@lip6.fr>
2007-09-15 15:02:56 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=3414

---
+++ tests/longint.c
@@ -0,0 +1,9 @@
+
+MODULE_PARM(cm206_base, "i");	/* base */
+
+
+static void do_cm206_request(request_queue_t * q)
+{
+	long int i;
+
+}
---
+++ tests/longint.cocci
@@ -0,0 +1,12 @@
+@ rule2 @
+identifier I;
+expression E;
+@@
+
+MODULE_PARM(I, E);
+
+@@
+identifier rule2.I;
+@@
+
+int I;
