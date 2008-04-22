75cec682a378bf1867e2b9f4fcfdb99e4bba7526
Henrik Stuart <hstuart@diku.dk>
2008-04-22 07:19:14 +0000

    Fixed location issues with python code
    svn path=/coccinelle/; revision=4247

--- coccilib/elems.py
+++ coccilib/elems.py
@@ -7,21 +7,21 @@ class Location:
 		self.column_end = column_end
 
 class ElemBase:
-	def __init__(self, loc):
-		self.location = loc
+	def __init__(self):
+		pass
 
 
 class Expression(ElemBase):
-	def __init__(self, expr, loc):
-		ElemBase.__init__(self, loc)
+	def __init__(self, expr):
+		ElemBase.__init__(self)
 		self.expr = expr
 
 	def __str__(self):
 		return self.expr
 
 class Identifier(ElemBase):
-	def __init__(self, ident, loc):
-		ElemBase.__init__(self, loc)
+	def __init__(self, ident):
+		ElemBase.__init__(self)
 		self.ident = ident
 
 	def __str__(self):
