ad75e11c19a940b02d4c414e90fed97fa49cfd2d
Yoann Padioleau <pad@cs.uiuc.edu>
2008-07-19 20:40:31 +0000

    add python files
    svn path=/coccinelle/; revision=4809

---
+++ python/no_pycocci.ml
@@ -0,0 +1,61 @@
+open Ast_c
+open Common
+open Pycocci_aux
+module StringMap = Map.Make (String)
+
+exception Pycocciexception
+
+let check_return_value v = failwith "no python"
+let check_int_return_value v = failwith "no python"
+
+let initialised = ref false
+
+let cocci_file_name = ref ""
+
+(* dealing with python modules loaded *)
+let get_module module_name = failwith "no python"
+
+let is_module_loaded module_name = failwith "no python"
+
+let load_module module_name = failwith "no python"
+(* end python module handling part *)
+
+(* initialisation routines *)
+let pycocci_init () = initialised := true
+
+(*let _ = pycocci_init ()*)
+(* end initialisation routines *)
+
+(* python interaction *)
+let split_fqn fqn = failwith "no python"
+
+let pycocci_get_class_type fqn = failwith "no python"
+
+let pycocci_instantiate_class fqn args = failwith "no python"
+
+(* end python interaction *)
+
+let inc_match = ref false
+
+let include_match v = failwith "no python"
+
+let build_method (mname, camlfunc, args) pymodule classx classdict =
+  failwith "no python"
+
+let build_class cname parent methods pymodule = failwith "no python"
+
+let has_environment_binding env name = failwith "no python"
+
+let get_cocci_file args = failwith "no python"
+
+let build_classes env = failwith "no python"
+
+let build_variable name value = failwith "no python"
+
+let contains_binding e (_,(r,m)) = failwith "no python"
+
+let construct_variables mv e = failwith "no python"
+
+let set_coccifile cocci_file =
+	cocci_file_name := cocci_file;
+	()
---
+++ python/pycocci_aux.ml
@@ -0,0 +1,57 @@
+open Ast_c
+open Common
+open Pycaml
+
+let rec exprrep expr = match expr with
+  Ast_c.Ident s -> s
+| Ast_c.Constant c -> constantrep c
+| Ast_c.FunCall (e,args) -> "TODO: FunCall"
+| Ast_c.CondExpr (e1,e2,e3) -> "TODO: CondExpr"
+| Ast_c.Sequence (e1,e2) -> "TODO: Sequence"
+| Ast_c.Assignment (e1,op,e2) -> "TODO: Assignment"
+| Ast_c.Postfix (e,op) -> "TODO: Postfix"
+| Ast_c.Infix (e,op) -> "TODO: Infix"
+| Ast_c.Unary (e,op) -> "TODO: Unary"
+| Ast_c.Binary (e1,op,e2) -> "TODO: Binary"
+| Ast_c.ArrayAccess (e1,e2) -> "TODO: ArrayAccess"
+| Ast_c.RecordAccess (e1,s) -> "TODO: RecordAccess"
+| Ast_c.RecordPtAccess (e,s) -> "TODO: RecordPtAccess"
+| Ast_c.SizeOfExpr e -> "TODO: SizeOfExpr"
+| Ast_c.SizeOfType t -> "TODO: SizeOfType"
+| Ast_c.Cast (t,e) -> "TODO: Cast"
+| Ast_c.StatementExpr c -> "TODO: StatementExpr"
+| Ast_c.Constructor (t,i) -> "TODO: Constructor"
+| Ast_c.ParenExpr e -> "TODO: ParenExpr"
+and constantrep c = match c with
+  Ast_c.String (s,isWchar) -> s 
+| Ast_c.MultiString -> "TODO: MultiString"
+| Ast_c.Char (s,isWchar) -> s
+| Ast_c.Int s -> s 
+| Ast_c.Float (s,t) -> s
+
+let call_pretty f a =
+  let str = ref ([] : string list) in
+  let pr_elem info = str := (Ast_c.str_of_info info) :: !str in
+  let pr_sp _ = () in
+  f pr_elem pr_sp a;
+  String.concat " " (List.rev !str)
+
+let stringrep mvb = match mvb with
+  Ast_c.MetaIdVal        s -> s
+| Ast_c.MetaFuncVal      s -> s
+| Ast_c.MetaLocalFuncVal s -> s
+| Ast_c.MetaExprVal      ((expr,_),[il]) -> (exprrep expr)
+| Ast_c.MetaExprVal	 e -> "TODO: <<MetaExprVal>>"
+| Ast_c.MetaExprListVal  expr_list -> "TODO: <<exprlist>>"
+| Ast_c.MetaTypeVal      typ -> call_pretty Pretty_print_c.pp_type_gen typ
+| Ast_c.MetaStmtVal      statement -> "TODO: stmt"
+| Ast_c.MetaParamVal     params -> "TODO: <<param>>"
+| Ast_c.MetaParamListVal params -> "TODO: <<paramlist>>"
+| Ast_c.MetaListlenVal n -> string_of_int n
+| Ast_c.MetaPosVal (pos1, pos2) ->
+    let print_pos = function
+	Ast_cocci.Real x -> string_of_int x
+      | Ast_cocci.Virt(x,off) -> Printf.sprintf "%d+%d" x off in
+    Common.sprintf ("pos(%s,%s)") (print_pos pos1) (print_pos pos2)
+| Ast_c.MetaPosValList positions -> "TODO: <<postvallist>>"
+
