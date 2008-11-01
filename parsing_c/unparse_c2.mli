0b29ed7ebb29c72cd7c093af8b1e19b31052a21d
Yoann Padioleau <pad@cs.uiuc.edu>
2008-11-01 20:46:20 +0000

    remove file because rename
    svn path=/coccinelle/; revision=5071

--- parsing_c/unparse_c2.mli
+++
@@ -1,7 +0,0 @@
-open Common
-
-type ppmethod = PPnormal | PPviastr
-
-(* program -> output filename (often "/tmp/output.c") -> unit *) 
-val pp_program : 
-  (Parse_c.toplevel2 * ppmethod) list -> filename -> unit
