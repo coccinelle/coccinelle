bc476abf4b4799c445fda7fc6fe696570920d4a9
Nicolas Palix <Nicolas.Palix@inria.fr>
2008-11-05 11:00:17 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=5102

--- ctl/ctl_engine.ml
+++ ctl/ctl_engine.ml
@@ -373,7 +373,7 @@ let print_graph grp required_states res str = function
 	      None -> "graphical_trace"
 	    | Some f -> f
 		  ) in
-	    let filename = Filename.temp_file file ".dot" in	   
+	    let filename = Filename.temp_file (file^":") ".dot" in	   
 	    graph_stack := filename :: !graph_stack;
 	    stepcnt := !stepcnt +1;
 	    G.print_graph grp
