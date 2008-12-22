dfa6db4b317cc9cb1d6575715101b6db63b3f166
Julia Lawall <julia.lawall@lip6.fr>
2008-12-22 18:03:20 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=5449

---
+++ tests/bad_define_parse.c
@@ -0,0 +1,5 @@
+/* I830 Video support */
+#define NEED_REPLIES                              /* ? */
+#define EXTENSION_PROC_ARGS void *
+#include "extnsionst.h"                   /* required */
+#include <X11/extensions/panoramiXproto.h>        /* required */
