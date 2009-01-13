22df9f00cc3c104c453c6b7d1797749c4af98a89
Nicolas Palix <Nicolas.Palix@inria.fr>
2009-01-13 15:30:03 +0000

    Add some statistics on stderr
    svn path=/coccinelle/; revision=5579

--- tools/gengraph/gengraph.ml
+++ tools/gengraph/gengraph.ml
@@ -1,13 +1,15 @@
 
 let existfile = ref ""
 let bugfile = ref ""
+let verbose = ref false
 
 let usage_msg =
   "Usage: " ^ Filename.basename Sys.argv.(0) ^ " -e <existfile> -b <bugfile>\n"
 
 let options = [
-  "-e",  Arg.Set_string existfile, "";
-  "-b",  Arg.Set_string bugfile, ""
+  "-e", Arg.Set_string existfile, "CSV file-exist list";
+  "-b", Arg.Set_string bugfile, "cleanup org file containing bug list";
+  "-v", Arg.Set verbose, "verbose mode"
 ]
 
 let _ =
@@ -17,5 +19,5 @@ let _ =
   let (max_ver, fileexist) = Exists.compute_exist ast_exist in
 (*     List.iter show_entry fileexist; *)
   let bfl = List.map (Bugs.compute_bug max_ver) bugs in
-(*     Graph.show_stat fileexist bfl; *)
+    Graph.show_stat !verbose max_ver fileexist bfl;
     Graph.draw_graph max_ver fileexist bfl
--- tools/gengraph/graph.ml
+++ tools/gengraph/graph.ml
@@ -28,14 +28,55 @@ let compute_bug_info fel bug =
 let compute_graph fel fbl =
     List.map (compute_bug_info fel) fbl
 
-let show_stat fel fbl =
+let cd_ratio max_ver bugs =
+  List.fold_left
+    (fun (i,j) (fc, c, fmin, bmin, bmax, fmax) ->
+       ((if (fmin = bmin && fmin != 0) then i+1 else i),
+	(if (fmax = bmax && fmax != max_ver) then j+1 else j))
+    ) (0,0) bugs
+
+let sl_ratio bugs =
+  List.fold_left
+    (fun (i,j) (fc, c, fmin, bmin, bmax, fmax) ->
+       let duration = bmax - bmin + 1 in
+       ((if (duration < i) then duration else i),
+	(if (duration > j) then duration else j))
+    ) (max_int, 0) bugs
+
+let verbose_stat bugs =
   List.iter (fun (fc, c, fmin, bmin, bmax, fmax) ->
 	       prerr_endline (fc ^ " - " ^ c ^
 				" fm: " ^ (string_of_int fmin) ^
 				" bm: " ^ (string_of_int bmin) ^
 				" bM: " ^ (string_of_int bmax) ^
 				" fM: " ^ (string_of_int fmax)))
-    (compute_graph fel fbl)
+    bugs
+
+let show_stat verbose max_ver fel fbl =
+  let fn = List.length fel in
+  let bn = List.length fbl in
+  let bugs = compute_graph fel fbl in
+  let (create, delete) = cd_ratio max_ver bugs in
+  let (shortest, longest) = sl_ratio bugs in
+    (if verbose then
+       verbose_stat bugs; prerr_newline ()
+    );
+    prerr_string ((string_of_int fn)^ " file(s) affected by ");
+    prerr_endline ((string_of_int bn)^ " bug(s)");
+    prerr_float (float_of_int bn/. float_of_int fn);
+    prerr_endline " bug(s) per file";
+    prerr_int create;
+    prerr_string " bug(s) (";
+    prerr_int (create * 100 / bn);
+    prerr_endline "%) introduced by a new file. ";
+    prerr_int delete;
+    prerr_string " bug(s) (";
+    prerr_int (delete * 100 / bn);
+    prerr_endline "%) removed by a file deletion.";
+    prerr_int shortest;
+    prerr_endline " version(s) for the shortest bug life.";
+    prerr_int longest;
+    prerr_endline " version(s) for the longest bug life."
 
 let draw_graph max fel fbl =
   let size = List.length fbl in
