1b41ba007bc70c5292913248bf86a143586d9146
Nicolas Palix <Nicolas.Palix@inria.fr>
2009-01-13 20:39:36 +0000

    Fix regression
    svn path=/coccinelle/; revision=5582

--- tools/gengraph/gengraph.ml
+++ tools/gengraph/gengraph.ml
@@ -7,7 +7,9 @@ let verbose = ref false
 let help = ref false
 
 let usage_msg =
-  "Usage: " ^ Filename.basename Sys.argv.(0) ^ " -e <existfile> -b <bugfile>\n"
+  "Usage: " ^ Filename.basename Sys.argv.(0) ^
+    " [-e <existfile> -b <bugfile>]" ^
+    " [-p <prefix> -o <orgfile>]\n"
 
 let options = [
   "-e", Arg.Set_string existfile, "file CSV file-exist list";
@@ -24,14 +26,13 @@ let _ =
   if (!help) then
     Arg.usage aligned usage_msg
   else
-    if (!orgfile != "") then
+    if ((String.length !orgfile) != 0) then
       let orgs = Org.parse_org !orgfile in
 	Org.show_org !prefix orgs
     else
       let ast_exist = Exists.parse_exist !existfile in
       let bugs = Bugs.parse_bugs !bugfile in
       let (max_ver, fileexist) = Exists.compute_exist ast_exist in
-	(*     List.iter show_entry fileexist; *)
       let bfl = List.map (Bugs.compute_bug max_ver) bugs in
 	Graph.show_stat !verbose max_ver fileexist bfl;
 	Graph.draw_graph max_ver fileexist bfl
