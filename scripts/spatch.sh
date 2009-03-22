c35a14711fd66de6522d1cfd2e9a96c9f86c899c
Nicolas Palix <Nicolas.Palix@inria.fr>
2009-03-22 18:17:15 +0000

    Switch to sh instead of bash - Add some defensive coding -- patch proposed by Thomas Klausner
    svn path=/coccinelle/; revision=5854

--- scripts/spatch.sh
+++ scripts/spatch.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 echo setting COCCINELLE_HOME=${COCCINELLE_HOME:=SHAREDIR}
 
-if [ ! -r $COCCINELLE_HOME/standard.iso ] ; then
+if [ ! -r "$COCCINELLE_HOME"/standard.iso ] ; then
     echo "There is no standard.iso in SHAREDIR."
     echo "Are you sure you run a properly installed version of spatch ?\n"
 else
 
  export COCCINELLE_HOME
- export LD_LIBRARY_PATH=$COCCINELLE_HOME:$LD_LIBRARY_PATH
- export PYTHONPATH=$COCCINELLE_HOME/python:$PYTHONPATH
+ export LD_LIBRARY_PATH="$COCCINELLE_HOME:$LD_LIBRARY_PATH"
+ export PYTHONPATH="$COCCINELLE_HOME/python:$PYTHONPATH"
  
- echo setting LD_LIBRARY_PATH=$LD_LIBRARY_PATH
- echo setting PYTHONPATH=$PYTHONPATH
+ echo setting LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
+ echo setting PYTHONPATH="$PYTHONPATH"
 
 fi
 
-$COCCINELLE_HOME/spatch.opt $*
+"$COCCINELLE_HOME"/spatch.opt "$@"
 
