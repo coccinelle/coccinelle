5cd5c2c837cef356b7a03a27ca960f9fdeedc5fb
Yoann Padioleau <pad@cs.uiuc.edu>
2009-04-23 20:30:28 +0000

    remove macros from apache
    svn path=/coccinelle/; revision=6339

--- standard.h
+++ standard.h
@@ -70,24 +70,13 @@
 // Generic macros
 // ****************************************************************************
 
-// ****************************************************************************
-// Yacc macros 
-// ****************************************************************************
+// this is defined by windows compiler, and so can not be found via a macro
+// after a -extract_macros
+#define __stdcall /*could: YACFE_ATTRIBUTE*/
+#define __declspec(a) 
 
-#define YY_PROTO(x) x 
-#define yyconst const 
-
-
-// ****************************************************************************
-// GNU Hello macros 
-// ****************************************************************************
-
-#define __getopt_argv_const const
-
-
-// ****************************************************************************
-// Gcc (as in the source of gcc code) macros 
-// ****************************************************************************
+#define WINAPI
+#define CALLBACK
 
 
 // ****************************************************************************
@@ -758,71 +747,3 @@ do {									\
 //#define	 ACPI_MODULE_NAME(x)
 
 
-
-
-// ****************************************************************************
-// Httpd (apache) macros 
-// ****************************************************************************
-
-#define AP_DECLARE(x) x
-#define PROXY_DECLARE(x) x
-#define CACHE_DECLARE(x) x
-#define DBD_DECLARE_NONSTD(x) x
-#define DAV_DECLARE(x) x
-#define APU_DECLARE(x) x
-#define APU_DECLARE_NONSTD(x) x
-#define APR_DECLARE(x) x
-#define AP_CORE_DECLARE(x) x
-#define AP_DECLARE_NONSTD(x) x
-#define AP_CORE_DECLARE_NONSTD(x) x
-#define APR_OPTIONAL_FN_TYPE(x) x
-#define DAV_DECLARE_NONSTD(x) x
-#define APR_DECLARE_NONSTD(x) x
-
-#define APU_DECLARE_DATA
-#define APR_THREAD_FUNC
-#define AP_DECLARE_DATA
-#define PROXY_DECLARE_DATA
-#define AP_MODULE_DECLARE_DATA
-#define APR_DECLARE_DATA
-
-
-
-#define APR_INLINE inline
-#define EXPORT static
-#define REGISTER register
-
-#define MODSSL_D2I_SSL_SESSION_CONST const 
-#define MODSSL_D2I_X509_CONST const
-#define MODSSL_D2I_PrivateKey_CONST const
-#define MODSSL_D2I_SSL_SESSION_CONST const
-
-#define STACK_OF(X509_NAME) X509_NAME
-
-#define MODSSL_PCHAR_CAST  (pchar)
-
-#define WINAPI
-//#define CALLBACK
-// generate false positive in Linux 
-#define APIENTRY
-#define __declspec(x) 
-#define __stdcall
-
-
-//#define module struct xxx
-
-#define APR_POOL_IMPLEMENT_ACCESSOR(shm)
-
-#define ADD_SUITE(suite) suite;
-
-// ****************************************************************************
-// CISCO vpn client macros 
-// ****************************************************************************
-
-// #define NOREGPARM 
-// #define IN
-// #define OUT
-// #define OPTIONAL
-
-#define likely(x) x
-#define unlikely(x) x
