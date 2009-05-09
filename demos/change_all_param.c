2627ac8b5a17c3ddb78dd2890ebbac73753946e3
Yoann Padioleau <pad@cs.uiuc.edu>
2009-05-09 14:59:18 +0000

    add demos/change_all_param
    svn path=/coccinelle/; revision=6579

---
+++ demos/change_all_param.c
@@ -0,0 +1,19 @@
+int foo(int a, int b)
+{
+	return a + b;
+}
+
+int bar(int a, int b, int c)
+{
+	return a + b + c;
+}
+
+void main(void)
+{
+	int res;
+	int x,y,y;
+
+	res = foo(x,y);
+
+	res = bar(x,y,z);
+}
