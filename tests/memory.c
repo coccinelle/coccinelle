911f36facf6fa6d20f5001fa936702fcb9dec57d
Julia Lawall <julia.lawall@lip6.fr>
2009-07-15 20:09:01 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=6808

--- tests/memory.c
+++ tests/memory.c
@@ -1,7 +1,10 @@
-#define SET_MAGIC(q,m)	((q)->magic = (m))
+#define QUEUE_MAGIC_FREE	0xf7e1c9a3
 #define BAD_MAGIC(q,m)	((q)->magic != (m))
+
+
 int __queue_add(Queue_t *queue, Scsi_Cmnd *SCpnt, int head)
 {
+	QE_t *q;
 	if (BAD_MAGIC(q, QUEUE_MAGIC_FREE))
 		BUG();
 }
--- tests/memory.res
+++ tests/memory.res
@@ -1,6 +1,9 @@
-#define SET_MAGIC(q,m)	((q)->magic = (m))
+#define QUEUE_MAGIC_FREE	0xf7e1c9a3
 #define BAD_MAGIC(q,m)	((q)->magic != (m))
+
+
 int __queue_add(Queue_t *queue, Scsi_Cmnd *SCpnt, int head)
 {
+	QE_t *q;
 	BUG_ON (BAD_MAGIC(q, QUEUE_MAGIC_FREE));
 }
