6cf31aef6c575a9e132701c8a854bb126d1876c4
Nicolas Palix <Nicolas.Palix@inria.fr>
2009-09-22 15:07:59 +0000

    Python coccilib cleanup
    svn path=/coccinelle/; revision=7223

---
+++ python/coccilib/org.py
@@ -0,0 +1,9 @@
+def print_main(p, msg="", color="ovl-face1") :
+	print "* TODO [[view:%s::face=%s::linb=%s::colb=%s::cole=%s][%s %s::%s]]" % (p[0].file,color,p[0].line,p[0].column,p[0].column_end,msg,p[0].file,p[0].line)
+
+def print_sec(p, msg="", color="ovl-face2") :
+	print "[[view:%s::face=%s::linb=%s::colb=%s::cole=%s][%s]]" % (p[0].file,color,p[0].line,p[0].column,p[0].column_end,msg)
+
+def print_secs(ps, msg="", color="ovl-face2") :
+	for i in ps:
+		print "[[view:%s::face=%s::linb=%s::colb=%s::cole=%s][%s]]" % (i.file,color,i.line,i.column,i.column_end,msg)
