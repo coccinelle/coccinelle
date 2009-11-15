d961236f575b9855f18c3eacb883f99a4a239243
Nicolas Palix <Nicolas.Palix@inria.fr>
2009-11-15 16:29:01 +0000

    Fix print_secs
    svn path=/coccinelle/; revision=7935

--- python/coccilib/org.py
+++ python/coccilib/org.py
@@ -1,5 +1,5 @@
 def build_link(p, msg, color) :
-	return "[[view:%s::face=%s::linb=%s::colb=%s::cole=%s][%s]]" % (p[0].file,color,p[0].line,p[0].column,p[0].column_end,msg)
+	return "[[view:%s::face=%s::linb=%s::colb=%s::cole=%s][%s]]" % (p.file,color,p.line,p.column,p.column_end,msg)
 
 def print_todo(p, msg="", color="ovl-face1") :
 	link = build_link(p, msg, color)
@@ -14,10 +14,10 @@ def print_link(p, msg="", color="ovl-face1") :
 #
 def print_main(msg, p, color="ovl-face1") :
 	oldmsgfmt = "%s %s::%s" % (msg,p[0].file,p[0].line)
-	print_todo(p, oldmsgfmt, color)
+	print_todo(p[0], oldmsgfmt, color)
 
 def print_sec(msg, p, color="ovl-face2") :
-	print_link(p, msg, color)
+	print_link(p[0], msg, color)
 
 def print_secs(msg, ps, color="ovl-face2") :
 	for i in ps:
