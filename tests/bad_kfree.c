2fdcffd5ee83f6494860b95920ca0804a2f0813e
Julia Lawall <julia.lawall@lip6.fr>
2010-03-27 18:03:56 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=9009

---
+++ tests/bad_kfree.c
@@ -0,0 +1,20 @@
+int main () {
+  for (i = 0; i < IVTV_VBI_FRAMES; i++) {
+    a = itv[i];
+    kfree(itv[i]);
+  }
+  print("foo",itv[i]);
+  print("foo",itv[i]);
+  a = itv[i];
+  itv[i]=12;
+  a = itv[i];
+}
+
+int bad () {
+  kfree(itv[i]);
+  print("foo",itv[i]);
+  print("foo",itv[i]);
+  a = itv[i];
+  itv[i]=12;
+  a = itv[i];
+}
