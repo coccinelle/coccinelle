5b8dec90583cd14bda963cf738c4444ab902e5cb
Nicolas Palix <Nicolas.Palix@inria.fr>
2010-06-18 15:55:06 +0000

    *** empty log message ***
    svn path=/coccinelle/; revision=10165

--- ocaml/coccilib.mli
+++ ocaml/coccilib.mli
@@ -1,4 +1,4 @@
-type pos = { current_element : string; line : string; col : string;
+type pos = { current_element : string; file: string ; line : string; col : string;
 	     line_end : string; col_end : string; }
 
 type param_type = Pos of pos list | Str of string
