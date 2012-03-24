a6688ebd45d19e8add70c51d22a5d658573ada88
Arie Middelkoop <amiddelk@gmail.com>
2012-03-25 00:16:57 +0100

    Fixed the tarball ocnstruction in 'release.nix'.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # build the 'configure' script
-
+echo "running aclocal (requires 'pkg-config' macros)"
 aclocal -I setup
+
+echo "running autoconf"
 autoconf
--- release.nix
+++ release.nix
@@ -33,10 +33,9 @@ let
       inherit versionSuffix;
 
       buildInputs = [
-        python texLiveFull
-        ocaml findlib menhir
-        ocaml_pcre ocaml_sexplib
-        ocaml_extlib pycaml
+        ocaml findlib menhir python
+        texLiveFull # for building the documentation
+	pkgconfig  # for the autoconf macros
       ];
 
       preDist = ''
