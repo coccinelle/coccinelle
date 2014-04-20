61059b9289935652a30e309679056979926977b1
julia <julia.lawall@lip6.fr>
2014-04-20 19:40:12 +0200

    PCRE regular expressions are not comparable.  Previously, we put them in the constraints of SmPL metavariables, they would end up in rule_elems, and then they woudl end up being compared in the CTL matching process.  The regular expressions are needed during the matching, and it would be costly to strip the rule_elems after the matching but before any possible comparison, or to statically create both an unstripped (for matching) and a stripped (for comparing) representation.  Instead, we just avoid the problem by putting the PCRE regular expressions in a hash table.
    Also fix a typo in changes.txt.

--- globals/regexp_pcre.ml
+++ globals/regexp_pcre.ml
@@ -1,18 +1,34 @@
-
 type regexp =
-    Pcre of Pcre.regexp
+    Pcre of int (* Pcre.regexp *)
   | Str of Str.regexp
+
+(* A table is used because PCRE regular expressions are not comparable.  They
+sit in constraints of rule_elems that get bound to exists v variables in
+the matching process.  It would be expensive to strip them at runtime, and
+complex to strip them statically, because + and - annotations are added
+by cocci_vs_c into the same rule_elems.  So instead we just replace them
+by integers, which are comparable. *)
+let pcre_table = Hashtbl.create 101
+let pcre_ctr = ref 0 
 
 let pcre_support = ref true
 
 let regexp string =
   if !pcre_support
-  then Pcre (Pcre.regexp string)
+  then
+    begin
+      let c = !pcre_ctr in
+      pcre_ctr := !pcre_ctr + 1;
+      Hashtbl.add pcre_table c (Pcre.regexp string);
+      Pcre c
+    end
   else Str (Str.regexp string)
 
 let string_match regexp string =
   match regexp with
-      Pcre regexp -> Pcre.pmatch ~rex:regexp string
+      Pcre regexp ->
+	let regexp = Hashtbl.find pcre_table regexp in
+	Pcre.pmatch ~rex:regexp string
     | Str regexp ->
       try
 	ignore(Str.search_forward regexp string 0);
