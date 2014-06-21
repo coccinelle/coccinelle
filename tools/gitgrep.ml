86694b66a3b5530b8de900b9c47926e94a19388a
julia <julia.lawall@lip6.fr>
2014-06-21 21:59:29 +0200

    added ability to read from stdin

--- tools/gitgrep.ml
+++ tools/gitgrep.ml
@@ -24,66 +24,129 @@ let matches pattern line =
 
 let res = ref []
 
-let scan dir pattern i =
+let changed = ref 0
+let badgits = ref []
+let add x = function (y::_) as a -> if x = y then a else x::a | _ -> [x]
+let too_many_changed = function Some n -> !changed > n | None -> false
+
+(*let scan dir pattern i maxlen =
   let rec loop skipping cl git =
     let line = input_line i in
     match Str.split space line with
-      ["commit";git] -> loop false true git
-    | "diff"::_ -> loop skipping false git
+      ["commit";newgit] ->
+	(if too_many_changed maxlen then badgits := add git !badgits);
+	changed := 0;
+	loop false true newgit
+    | "diff"::_ ->
+	(if too_many_changed maxlen then badgits := add git !badgits);
+	changed := 0;
+	loop skipping false git
     | _ ->
-	if String.length line > 0 && not skipping &&
-	  ((String.get line 0 = '-' && dir = Minus) or
-	   (String.get line 0 = '+' && dir = Plus) or
-	   (cl && dir = ChangeLog) or
-	   (not (String.get line 0 = '-') && not (String.get line 0 = '+') &&
-	    dir = Context)) &&
-	  matches pattern line
-	then (res := Git(git)::!res; loop true cl git)
-	else loop skipping cl git in
-  loop false false ""
+	if String.length line = 0
+	then loop skipping cl git
+	else
+	  begin
+	    let start = String.get line 0 in
+	    (if start = '-' or start = '+' then changed := !changed + 1);
+	    if not skipping &&
+	      ((start = '-' && dir = Minus) or (start = '+' && dir = Plus) or
+	       (cl && dir = ChangeLog) or
+	       (not (start = '-') && not (start = '+') && dir = Context)) &&
+	      matches pattern line
+	    then (res := Git(git)::!res; loop true cl git)
+	    else loop skipping cl git
+	  end in
+  loop false false ""*)
 
-(* for Minus and Plus directions only *)
-let scan_grouped dir pattern i =
-  let block = ref 0 in
-  (* mp = true in minus-plus region *)
-  let rec loop mp git =
+let scan allpatterns i maxlen =
+  let rec loop cl git patterns =
     let line = input_line i in
     match Str.split space line with
-      ["commit";git] -> loop false git
-    | "diff"::_ -> loop false git
+      ["commit";newgit] ->
+	(if too_many_changed maxlen
+	then badgits := add git !badgits
+	else if patterns = [] then res := Git(git)::!res);
+	changed := 0;
+	loop true newgit allpatterns
+    | "diff"::_ ->
+	(if too_many_changed maxlen then badgits := add git !badgits);
+	changed := 0;
+	loop false git patterns
+    | _ ->
+	if String.length line = 0
+	then loop cl git patterns
+	else
+	  begin
+	    let start = String.get line 0 in
+	    (if start = '-' or start = '+' then changed := !changed + 1);
+	    let remaining_patterns =
+	      List.filter
+		(function (dir,pattern) ->
+		  not (* argument is true if match succeeds *)
+		    (if ((start = '-' && dir = Minus) or
+			 (start = '+' && dir = Plus) or
+			 (cl && dir = ChangeLog) or
+			 (not (start = '-') && not (start = '+') &&
+			  dir = Context))
+		    then matches pattern line
+		    else false))
+		patterns in
+	    loop cl git remaining_patterns
+	  end in
+  loop false "" allpatterns
+
+(* for Minus and Plus directions only *)
+let scan_grouped allpatterns i maxlen =
+  let block = ref 0 in
+  (* mp = true in minus-plus region *)
+  let rec loop mp git patterns =
+    let line = input_line i in
+    match Str.split space line with
+      ["commit";newgit] ->
+	(if too_many_changed maxlen
+	then badgits := add git !badgits
+	else if patterns = []
+	then res := Block(!block,git) :: !res);
+	changed := 0;
+	block := 0;
+	loop false newgit allpatterns
+    | "diff"::_ ->
+	(if too_many_changed maxlen
+	then badgits := add git !badgits
+	else if patterns = []
+	then res := Block(!block,git) :: !res);
+	changed := 0;
+	loop false git patterns
     | _ ->
 	if String.length line > 0
 	then
 	    let first_char = String.get line 0 in
-	    let new_mp =
+	    (if first_char = '-' or first_char = '+'
+	    then changed := !changed + 1);
+	    let (new_mp,patterns) =
 	      match first_char with
-		'-' | '+' -> (if not mp then block := !block + 1; true)
-	      |	_ -> false in
-	    match (first_char,dir) with
-	      ('-',Minus) | ('+',Plus) ->
-		let info = Block(!block,git) in
-		(if matches pattern line && not (List.mem info !res)
-		then res := info::!res);
-		loop new_mp git
-	    | _ -> loop new_mp git
-	else loop mp git in
-  loop false ""
-
-let scan_line max i =
-  let rec loop skipping num git =
-    let line = input_line i in
-    match Str.split space line with
-      ["commit";git1] ->
-	loop false (-1) git1
-    | "diff"::_ ->
-	if num > max && not skipping
-	then (res:=Git(git)::!res;loop true (num+1) git)
-	else loop skipping (if num = (-1) then 1 else num+1) git
-    | _ ->
-	if num > max && not skipping
-	then (res:=Git(git)::!res;loop true (num+1) git)
-	else loop skipping (if num = (-1) then num else num+1) git in
-  loop false (-1) ""
+		'-' | '+' ->
+		  if not mp
+		  then
+		    begin
+		      (if patterns = []
+		      then res := Block(!block,git) :: !res);
+		      block := !block + 1;
+		      (true,allpatterns)
+		    end
+		  else (true,patterns)
+	      |	_ -> (false,patterns) in
+	    let remaining_patterns =
+	      List.filter
+		(function (dir,pattern) ->
+		  not (* argument is true if the pattern matches *)
+		    (match (first_char,dir) with
+		      ('-',Minus) | ('+',Plus) -> matches pattern line
+		    | _ -> false))
+		patterns in
+	    loop new_mp git remaining_patterns
+	else loop mp git patterns in
+  loop false "" allpatterns
 
 let dot = Str.regexp "\\."
 
@@ -107,68 +170,47 @@ let open_git file =
 	open_in tmp
     | _ -> open_in file
 
+let version = ref None
+
+let make_pattern s = Str.regexp (Printf.sprintf "\\b%s\\b" s)
+
 let rec split_args = function
     [] -> []
   | "-grouped"::rest   -> grouped := true; split_args rest
   | "-maxlen"::len::rest -> maxlen := Some (int_of_string len); split_args rest
-  | "-"::pattern::rest -> (Minus,Str.regexp pattern) :: split_args rest
-  | "+"::pattern::rest -> (Plus,Str.regexp pattern) :: split_args rest
-  | "@"::pattern::rest -> (Context,Str.regexp pattern) :: split_args rest
-  | "C"::pattern::rest -> (ChangeLog,Str.regexp pattern) :: split_args rest
+  | "-version"::v::rest -> version := Some v; split_args rest
+  | "-"::pattern::rest -> (Minus,make_pattern pattern) :: split_args rest
+  | "+"::pattern::rest -> (Plus,make_pattern pattern) :: split_args rest
+  | "@"::pattern::rest -> (Context,make_pattern pattern) :: split_args rest
+  | "C"::pattern::rest -> (ChangeLog,make_pattern pattern) :: split_args rest
   | _ -> failwith "bad argument list"
 
-let process_one (dir,pattern) version =
+let process patterns version maxlen =
   res := [];
-  let i = open_git version in
+  let i =
+    match version with Some version -> open_git version | None -> stdin in
   try
-    if !grouped && (dir = Minus or dir = Plus)
-    then scan_grouped dir pattern i
-    else scan dir pattern i
-  with End_of_file -> (close_in i; List.rev !res)
-
-let process_len max version =
-  res := [];
-  let i = open_git version in
-  try scan_line max i
-  with End_of_file -> (close_in i; List.rev !res)
-
-let inter l1 l2 =
-  List.rev
-    (List.fold_left
-       (function prev ->
-	 function
-	     (Git(git)) as x ->
-	       let rec loop = function
-		   [] -> prev
-		 | Git(git1)::rest when git = git1 -> x::prev
-		 | Block(b1,git1)::rest when git = git1 -> Block(b1,git1)::prev
-		 | _::rest -> loop rest in
-	       loop l2
-	   | (Block(block,git)) as x ->
-	       let rec loop = function
-		   [] -> prev
-		 | Git(git1)::rest when git = git1 -> x::prev
-		 | Block(b1,git1)::rest when block = b1 && git = git1 ->
-		     Block(b1,git1)::prev
-		 | _::rest -> loop rest in
-	       loop l2)
-       [] l1)
+    if !grouped
+    then scan_grouped patterns i maxlen
+    else scan patterns i maxlen
+  with End_of_file ->
+   ((match version with Some _ -> close_in i | None -> ()); List.rev !res)
 
 let _ =
-  if Array.length Sys.argv < 4
+  if Array.length Sys.argv < 3
   then failwith "arguments: -/+/@/C pattern -/+/@/C pattern ... version";
   let args = List.tl(Array.to_list Sys.argv) in
-  let version = List.hd(List.rev args) in
-  let pairs = List.rev(List.tl(List.rev args)) in
-  let requirements = split_args pairs in
+  let requirements = split_args args in
+  let version = !version in
+  (if !grouped
+  then
+    if List.exists (function (dir,_) -> not (dir = Minus or dir = Plus))
+	requirements
+    then
+      failwith "only minus and plus requirements allowed in the grouped case");
   let res =
     List.map (function Git x -> x | Block (_,x) -> x)
-      (List.fold_left
-	 (function all ->
-	   function pattern ->
-	     inter (process_one pattern version) all)
-	 (process_one (List.hd requirements) version)
-	 (List.tl requirements)) in
+      (process requirements version !maxlen) in
   let res =
     if !grouped
     then
@@ -178,10 +220,5 @@ let _ =
 	     function x -> if List.mem x prev then prev else x::prev)
 	   [] res)
     else res in
-  let res =
-    match !maxlen with
-      None -> res
-    | Some max ->
-	let badgits = process_len max version in
-	List.filter (function x -> not(List.mem (Git(x)) badgits)) res in
+  let res = List.filter (function x -> not(List.mem x !badgits)) res in
   List.iter (function name -> Printf.printf "%s\n" name) res
