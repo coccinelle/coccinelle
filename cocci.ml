d1370b9ec2da4df5dfce0a15be1dba6887850414
Julia Lawall <julia@cocci>
2015-02-01 22:32:20 +0100

    fix indentation

--- cocci.ml
+++ cocci.ml
@@ -113,11 +113,11 @@ let ast_to_flow_with_error_messages a =
 let ctls_of_ast2 ast (ua,fua,fuas) pos =
   List.map2
     (function ast -> function (ua,fua,fuas,pos) ->
-      let ast1 = if !Flag_cocci.popl
-                    then Popl.popl ast
-	                  else Asttoctl2.asttoctl ast (ua,fua,fuas) pos in
-      List.combine ast1 (Asttomember.asttomember ast ua)
-	    )
+      let ast1 =
+	if !Flag_cocci.popl
+	then Popl.popl ast
+	else Asttoctl2.asttoctl ast (ua,fua,fuas) pos in
+      List.combine ast1 (Asttomember.asttomember ast ua))
     ast (Common.combine4 ua fua fuas pos)
 
 let ctls_of_ast ast ua pl =
