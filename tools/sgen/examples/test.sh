b9796287e9f08aa7390988df0bc087d2cae5e177
Chi Pham <chidaph@gmail.com>
2015-03-17 18:00:28 +0000

    sgen: renamed test files to cocci extension to make them spatchable.
    added some documentation for "testing".

--- tools/sgen/examples/test.sh
+++ tools/sgen/examples/test.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # preliminary test script
-# simply runs sgen on each .cocci/.config combo and compares with .out file.
+# simply runs sgen on each .cocci/.config combo and compares with _.cocci file.
 
 # The sgen program
 SGEN=sgen
 
 START=$(date +%s)
 diffs=0
-for f in ./*.cocci
+for f in ./*[!'_'].cocci
 do
    filename=$(basename "$f")
    actual="${filename}f"
-   expected="${filename%.*}.sgencocci"
+   expected="${filename%.*}_.cocci"
 
    sgen ${filename} -o ${actual}
    if diff ${expected} ${actual} > /dev/null ; then
