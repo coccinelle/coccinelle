9dfbfbfb895cbb2f10271aceab0da0d3fbadb85b
Chi Pham <fempsci@gmail.com>
2015-03-31 20:53:43 +0100

    sgen: fixed code duplication TODO in pos generation.
    The mcode typing somehow magically works now.
    Maybe because of OCaml version upgrade?

--- tools/sgen/source/position_generator.ml
+++ tools/sgen/source/position_generator.ml
@@ -36,6 +36,12 @@ let make_pos (_, arity, info, mcodekind, _, adj) snp =
 let wrap a snp = Some (Ast0.wrap a, snp)
 let all_same = function [] -> true | x :: xs -> List.for_all (( = ) x) xs
 
+(* given mcode + snapshot, returns mcode with added metapos + new snapshot *)
+let mcode_pos ((x, a, info, mc, pos, q) as mco) snp =
+  if a = Ast0.OPT then (mco, snp) else
+  let (newpos, snp) = make_pos mco snp in
+  ((x, a, info, mc, ref (newpos :: !pos), q), snp)
+
 (* Auxiliary helpers for structures that largely follow the same format *)
 let rec exp_one exp fn snp =
   match expression_pos exp snp with
@@ -50,65 +56,21 @@ and exp_two exp1 exp2 fn snp =
        | None -> None
        | Some (a, snp) -> wrap (fn exp1 a) snp)
 
-(* Mcode handlers *)
-(* TODO: Somehow convince the OCaml typing system that it does, in fact, not
- * matter which type the mcode has; the end result should be the same.
- *)
-and string_mcode_pos ((x, a, info, mc, pos, q) as mco) snp =
-  if a = Ast0.OPT then (mco, snp) else
-  let (newpos, snp) = make_pos mco snp in
-  ((x, a, info, mc, ref (newpos :: !pos), q), snp)
-
-and meta_mcode_pos ((x, a, info, mc, pos, q) as mco) snp =
-  if a = Ast0.OPT then (mco, snp) else
-  let (newpos, snp) = make_pos mco snp in
-  ((x, a, info, mc, ref (newpos :: !pos), q), snp)
-
-and constant_pos ((x, a, info, mc, pos, q) as mco) snp =
-  if a = Ast0.OPT then (mco, snp) else
-  let (newpos, snp) = make_pos mco snp in
-  ((x, a, info, mc, ref (newpos :: !pos), q), snp)
-
-and binary_pos ((x, a, info, mc, pos, q) as mco) snp =
-  if a = Ast0.OPT then (mco, snp) else
-  let (newpos, snp) = make_pos mco snp in
-  ((x, a, info, mc, ref (newpos :: !pos), q), snp)
-
-and unary_pos ((x, a, info, mc, pos, q) as mco) snp =
-  if a = Ast0.OPT then (mco, snp) else
-  let (newpos, snp) = make_pos mco snp in
-  ((x, a, info, mc, ref (newpos :: !pos), q), snp)
-
-and fix_pos ((x, a, info, mc, pos, q) as mco) snp =
-  if a = Ast0.OPT then (mco, snp) else
-  let (newpos, snp) = make_pos mco snp in
-  ((x, a, info, mc, ref (newpos :: !pos), q), snp)
-
-and assign_pos ((x, a, info, mc, pos, q) as mco) snp =
-  if a = Ast0.OPT then (mco, snp) else
-  let (newpos, snp) = make_pos mco snp in
-  ((x, a, info, mc, ref (newpos :: !pos), q), snp)
-
-and inc_file_pos ((x, a, info, mc, pos, q) as mco) snp =
-  if a = Ast0.OPT then (mco, snp) else
-  let (newpos, snp) = make_pos mco snp in
-  ((x, a, info, mc, ref (newpos :: !pos), q), snp)
-
 (* generate a position for an identifier.
  * Always possible! but not done in optional expressions *)
 and ident_pos i snp =
   match Ast0.unwrap i with
   | Ast0.Id mc ->
-      let (mc, snp) = string_mcode_pos mc snp in
+      let (mc, snp) = mcode_pos mc snp in
       (Ast0.wrap (Ast0.Id(mc)), snp)
   | Ast0.MetaId(metamc, i, s, p) ->
-      let (metamc, snp) = meta_mcode_pos metamc snp in
+      let (metamc, snp) = mcode_pos metamc snp in
       (Ast0.wrap (Ast0.MetaId(metamc, i, s, p)), snp)
   | Ast0.MetaFunc(metamc, i, p) ->
-      let (metamc, snp) = meta_mcode_pos metamc snp in
+      let (metamc, snp) = mcode_pos metamc snp in
       (Ast0.wrap (Ast0.MetaFunc(metamc, i, p)), snp)
   | Ast0.MetaLocalFunc(metamc, i, p) ->
-      let (metamc, snp) = meta_mcode_pos metamc snp in
+      let (metamc, snp) = mcode_pos metamc snp in
       (Ast0.wrap (Ast0.MetaLocalFunc(metamc, i, p)), snp)
   | Ast0.DisjId _ -> (i, snp)
   | Ast0.OptIdent (id) ->
@@ -119,7 +81,7 @@ and ident_pos i snp =
       (Ast0.wrap (Ast0.UniqueIdent (id)), snp)
   | Ast0.AsIdent(id1, id2) -> failwith "Should only be in metavars"
 
-(*TODO: fix the disjunction thing. Usually we don't want to put positions
+(* TODO: fix the disjunction thing. Usually we don't want to put positions
  * at the types, but we can be forced to if there are disjunctions with
  * types that use SmPL pattern-matching.
  *)
@@ -132,7 +94,7 @@ and type_pos t snp =
                      "in position generator.")
   | _ -> None
 
-(*TODO: fix the disjunction thing. Usually we don't want to put positions
+(* TODO: fix the disjunction thing. Usually we don't want to put positions
  * at the cases, but we can be forced to if there are disjunctions with
  * cases that use SmPL pattern-matching.
  * NB: make sure that the statement dots in the case_line cases are generated
@@ -166,7 +128,7 @@ and declaration_pos d snp =
       wrap (Ast0.UnInit(st, ty, id, sem)) snp
   | Ast0.TyDecl _ -> failwith "tydecl"
   | Ast0.Typedef (tm, tc, tc2, sem) ->
-      let (mc, snp) = string_mcode_pos tm snp in
+      let (mc, snp) = mcode_pos tm snp in
       wrap (Ast0.Typedef (mc,tc,tc2,sem)) snp
   | Ast0.MacroDecl (id,lp,ed,rp,sem) ->
       let (id, snp) = ident_pos id snp in
@@ -194,11 +156,11 @@ and forinfo_pos f snp =
       (match expression_pos exp snp with
        | Some (a, snp) -> wrap (Ast0.ForExp(Some a, sem)) snp
        | None ->
-           let (m, snp) = string_mcode_pos sem snp in
+           let (m, snp) = mcode_pos sem snp in
            wrap (Ast0.ForExp(Some exp, m)) snp
       )
   | Ast0.ForExp (None, sem) ->
-      let (m,snp) = string_mcode_pos sem snp in
+      let (m,snp) = mcode_pos sem snp in
       wrap (Ast0.ForExp (None, m)) snp
   | Ast0.ForDecl (bef, decl) ->
       (match declaration_pos decl snp with
@@ -214,16 +176,16 @@ and expression_pos e snp =
       let (id, snp) = ident_pos id snp in
       wrap (Ast0.Ident(id)) snp
   | Ast0.Constant(cmc) ->
-      let (cmc, snp) = constant_pos cmc snp in
+      let (cmc, snp) = mcode_pos cmc snp in
       wrap (Ast0.Constant(cmc)) snp
   | Ast0.StringConstant(q1, sd, q2) ->
-      let (q2, snp) = string_mcode_pos q2 snp in
+      let (q2, snp) = mcode_pos q2 snp in
       wrap (Ast0.StringConstant(q1, sd, q2)) snp
   | Ast0.FunCall(exp, lp, expdots, rp) ->
       let fn x = Ast0.FunCall(x, lp, expdots, rp) in
       (match exp_one exp fn snp with
        | None ->
-           let (m,snp) = string_mcode_pos lp snp in
+           let (m,snp) = mcode_pos lp snp in
            wrap (Ast0.FunCall(exp, m, expdots, rp)) snp
        | a -> a
       )
@@ -231,7 +193,7 @@ and expression_pos e snp =
       let fn x y =  Ast0.Assignment(x, amc, y, st) in
       (match exp_two exp1 exp2 fn snp with
        | None ->
-           let (m,snp) = assign_pos amc snp in
+           let (m,snp) = mcode_pos amc snp in
            wrap (Ast0.Assignment(exp1, m, exp2, st)) snp
        | a -> a
       )
@@ -239,7 +201,7 @@ and expression_pos e snp =
       let fn x y = Ast0.Sequence(x, com, y) in
       (match exp_two exp1 exp2 fn snp with
        | None ->
-           let (m,snp) = string_mcode_pos com snp in
+           let (m,snp) = mcode_pos com snp in
            wrap (Ast0.Sequence(exp1,m,exp2)) snp
        | a -> a
       )
@@ -247,7 +209,7 @@ and expression_pos e snp =
       let fn x y = Ast0.CondExpr(x, why, expopt, colon, y) in
       (match exp_two exp1 exp2 fn snp with
        | None ->
-           let (m,snp) = string_mcode_pos why snp in
+           let (m,snp) = mcode_pos why snp in
            wrap (Ast0.CondExpr(exp1, m, expopt, colon, exp2)) snp
        | a -> a
       )
@@ -255,7 +217,7 @@ and expression_pos e snp =
       let fn x = Ast0.Postfix(x, fixmc) in
       (match exp_one exp fn snp with
        | None ->
-           let (m,snp) = fix_pos fixmc snp in
+           let (m,snp) = mcode_pos fixmc snp in
            wrap (Ast0.Postfix(exp,m)) snp
        | a -> a
       )
@@ -263,7 +225,7 @@ and expression_pos e snp =
       let fn x = Ast0.Infix(x, fixmc) in
       (match exp_one exp fn snp with
        | None ->
-           let (m,snp) = fix_pos fixmc snp in
+           let (m,snp) = mcode_pos fixmc snp in
            wrap (Ast0.Infix(exp,m)) snp
        | a -> a
       )
@@ -271,7 +233,7 @@ and expression_pos e snp =
       let fn x = Ast0.Unary(x, unmc) in
       (match exp_one exp fn snp with
        | None ->
-           let (m,snp) = unary_pos unmc snp in
+           let (m,snp) = mcode_pos unmc snp in
            wrap (Ast0.Unary(exp,m)) snp
        | a -> a
       )
@@ -279,7 +241,7 @@ and expression_pos e snp =
       let fn x y = Ast0.Binary(x, bin, y) in
       (match exp_two exp1 exp2 fn snp with
        | None ->
-           let (m,snp) = binary_pos bin snp in
+           let (m,snp) = mcode_pos bin snp in
            wrap (Ast0.Binary(exp1, m, exp2)) snp
        | a -> a
       )
@@ -287,7 +249,7 @@ and expression_pos e snp =
       let fn x y = Ast0.Nested(x, bin, y) in
       (match exp_two exp1 exp2 fn snp with
        | None ->
-           let (m,snp) = binary_pos bin snp in
+           let (m,snp) = mcode_pos bin snp in
            wrap (Ast0.Nested(exp1, m, exp2)) snp
        | a -> a
       )
@@ -295,7 +257,7 @@ and expression_pos e snp =
       let fn x = Ast0.Paren(lp, x, rp) in
       (match exp_one exp fn snp with
        | None ->
-           let (m,snp) = string_mcode_pos lp snp in
+           let (m,snp) = mcode_pos lp snp in
            wrap (Ast0.Paren(m,exp,rp)) snp
        | a -> a
       )
@@ -303,7 +265,7 @@ and expression_pos e snp =
       let fn x y = Ast0.ArrayAccess(x, lb, y, rb) in
       (match exp_two arrexp exp fn snp with
        | None ->
-           let (m,snp) = string_mcode_pos lb snp in
+           let (m,snp) = mcode_pos lb snp in
            wrap (Ast0.ArrayAccess(arrexp, m, exp,rb)) snp
        | a -> a
       )
@@ -328,27 +290,27 @@ and expression_pos e snp =
       let fn x = Ast0.Cast(lp, typec, rp, exp) in
       (match exp_one exp fn snp with
        | None ->
-           let (m,snp) = string_mcode_pos rp snp in
+           let (m,snp) = mcode_pos rp snp in
            wrap (Ast0.Cast(lp, typec, m,exp)) snp
        | a -> a
       )
   | Ast0.SizeOfExpr(sizeofmc, exp) ->
-      let (sizeofmc, snp) = string_mcode_pos sizeofmc snp in
+      let (sizeofmc, snp) = mcode_pos sizeofmc snp in
       wrap (Ast0.SizeOfExpr (sizeofmc, exp)) snp
   | Ast0.SizeOfType(sizeofmc, lp, typec, rp) ->
       let _ = type_pos typec snp in (*sanity check for disj*)
-      let (sizeofmc, snp) = string_mcode_pos sizeofmc snp in
+      let (sizeofmc, snp) = mcode_pos sizeofmc snp in
       wrap (Ast0.SizeOfType (sizeofmc, lp, typec, rp)) snp
   | Ast0.TypeExp(typec) -> type_pos typec snp (*sanity check, always None*)
   | Ast0.Constructor(lp, typec, rp, init) ->
       let _ = type_pos typec snp in (*sanity check for disj*)
-      let (lp, snp) = string_mcode_pos lp snp in
+      let (lp, snp) = mcode_pos lp snp in
       wrap (Ast0.Constructor (lp, typec, rp, init)) snp
   | Ast0.MetaErr (mc, co, pu) -> (*is this ever within the rule cody?*)
-      let (mc, snp) = meta_mcode_pos mc snp in
+      let (mc, snp) = mcode_pos mc snp in
       wrap (Ast0.MetaErr (mc, co, pu)) snp
   | Ast0.MetaExpr(mc, co, ty, fo, pu) ->
-      let (mc, snp) = meta_mcode_pos mc snp in
+      let (mc, snp) = mcode_pos mc snp in
       wrap (Ast0.MetaExpr(mc, co, ty, fo, pu)) snp
   | Ast0.UniqueExp e -> exp_one e (fun x -> Ast0.UniqueExp x) snp
   | Ast0.OptExp e -> exp_one e (fun x -> Ast0.OptExp x) snp
@@ -372,7 +334,7 @@ and statement_pos s snp =
   | Ast0.TopInit _ -> None
 
   | Ast0.Include (incmc,filemc) ->
-      let (filemc, snp) = inc_file_pos filemc snp in
+      let (filemc, snp) = mcode_pos filemc snp in
       wrap (Ast0.Include(incmc, filemc)) snp
   | Ast0.Undef (defmc, id) ->
       let (id, snp) = ident_pos id snp in
@@ -398,7 +360,7 @@ and statement_pos s snp =
       (match expression_pos e snp with
        | Some (v, sn) -> wrap (Ast0.ExprStatement(Some v, sem)) sn
        | None ->
-           let (m,snp) = string_mcode_pos sem snp in
+           let (m,snp) = mcode_pos sem snp in
            wrap (Ast0.ExprStatement(Some e, m)) snp
       )
   | Ast0.Exp e ->
@@ -415,7 +377,7 @@ and statement_pos s snp =
       (match expression_pos exp snp with
        | Some (v, sn) -> wrap (Ast0.IfThen(ifm, l, v, r, st, a)) sn
        | None ->
-           let (ifm, snp) = string_mcode_pos ifm snp in
+           let (ifm, snp) = mcode_pos ifm snp in
           wrap (Ast0.IfThen(ifm, l, exp, r, st, a)) snp
       )
   | Ast0.IfThenElse (ifm, l, exp, r, s1, e, s2, a) ->
@@ -423,21 +385,21 @@ and statement_pos s snp =
        | Some (v, snp) ->
            wrap (Ast0.IfThenElse(ifm,l,v,r,s1,e,s2,a)) snp
        | None ->
-           let (ifm, snp) = string_mcode_pos ifm snp in
+           let (ifm, snp) = mcode_pos ifm snp in
            wrap (Ast0.IfThenElse(ifm,l,exp,r,s1,e,s2,a)) snp
       )
   | Ast0.While (whmc, l, exp, r, s, a) ->
       (match expression_pos exp snp with
        | Some (v, snp) -> wrap (Ast0.While(whmc, l, v, r, s, a)) snp
        | None ->
-           let (whmc, snp) = string_mcode_pos whmc snp in
+           let (whmc, snp) = mcode_pos whmc snp in
            wrap (Ast0.While(whmc, l, exp, r, s, a)) snp
       )
   | Ast0.Do (d, s, whmc, l, exp, r, sem) ->
       (match expression_pos exp snp with
        | Some (v,snp) -> wrap (Ast0.Do(d, s, whmc, l, v, r, sem)) snp
        | None ->
-           let (d, snp) = string_mcode_pos d snp in
+           let (d, snp) = mcode_pos d snp in
            wrap (Ast0.Do(d, s, whmc, l, exp, r, sem)) snp
       )
   | Ast0.For (fo,lp, fi,expo1,sem,expo2,rp,stmt,a) ->
@@ -445,7 +407,7 @@ and statement_pos s snp =
        | Some (f,snp) ->
            wrap (Ast0.For (fo,lp, f, expo1, sem, expo2, rp, stmt, a)) snp
        | None ->
-           let (fo, snp) = string_mcode_pos fo snp in
+           let (fo, snp) = mcode_pos fo snp in
            wrap (Ast0.For(fo, lp,fi,expo1,sem,expo2,rp,stmt,a)) snp
       )
   | Ast0.Iterator (id,lp,expdots,rp,stmt,a) ->
@@ -458,7 +420,7 @@ and statement_pos s snp =
        | Some (v, snp) ->
            wrap (Ast0.Switch(sw, lp, v, rp, lb, sd, cd, rb)) snp
        | None ->
-           let (sw, snp) = string_mcode_pos sw snp in
+           let (sw, snp) = mcode_pos sw snp in
            wrap (Ast0.Switch(sw, lp, exp, rp, lb, sd, cd,rb)) snp
       )
   | Ast0.Label (id,col) ->
@@ -468,20 +430,20 @@ and statement_pos s snp =
       let (id, snp) = ident_pos id snp in
       wrap (Ast0.Goto (goto, id, sem)) snp
   | Ast0.Break (bmc,sem) ->
-      let (bmc, snp) = string_mcode_pos bmc snp in
+      let (bmc, snp) = mcode_pos bmc snp in
       wrap (Ast0.Break(bmc,sem)) snp
   | Ast0.Continue (cmc,sem) ->
-      let (cmc, snp) = string_mcode_pos cmc snp in
+      let (cmc, snp) = mcode_pos cmc snp in
       wrap (Ast0.Continue(cmc,sem)) snp
   | Ast0.ReturnExpr (retmc, exp, sem) ->
       (match expression_pos exp snp with
        | Some (v,snp) -> wrap (Ast0.ReturnExpr(retmc, v, sem)) snp
        | None ->
-           let (retmc, snp) = string_mcode_pos retmc snp in
+           let (retmc, snp) = mcode_pos retmc snp in
            wrap (Ast0.ReturnExpr(retmc, exp, sem)) snp
       )
   | Ast0.Return (retmc,sem) ->
-      let (retmc, snp) = string_mcode_pos retmc snp in
+      let (retmc, snp) = mcode_pos retmc snp in
       wrap (Ast0.Return(retmc,sem)) snp
   | Ast0.FunDecl (b, f, id, lp, ps, op, rp, lb, sd, rb, a) ->
       let (id, snp) = ident_pos id snp in
