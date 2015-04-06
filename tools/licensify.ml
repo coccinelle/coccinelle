4acfb7e1e9e04f7256a314cb87bd67cb108d0beb
julia <julia.lawall@lip6.fr>
2015-04-06 19:59:58 +0200

    update date

--- tools/licensify.ml
+++ tools/licensify.ml
@@ -1,5 +1,5 @@
 let lines =
-["Copyright 2012-2014, INRIA";
+["Copyright 2012-2015, Inria";
 "Julia Lawall, Gilles Muller";
 "Copyright 2010-2011, INRIA, University of Copenhagen";
 "Julia Lawall, Rene Rydhof Hansen, Gilles Muller, Nicolas Palix";
