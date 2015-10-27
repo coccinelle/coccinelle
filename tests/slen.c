6ea7e45b2ddeffe26b213ab884b82cc02f38309c
julia <julia.lawall@lip6.fr>
2015-10-27 21:07:03 +0100

    tests for statement list length

---
+++ tests/slen.c
@@ -0,0 +1,18 @@
+int main() {
+  if (x) {
+    one();
+  }
+  if (x) {
+    one();
+    two();
+    three();
+  }
+  if (x) {
+    one();
+    two();
+    three();
+    four();
+    five();
+  }
+  end();
+}
