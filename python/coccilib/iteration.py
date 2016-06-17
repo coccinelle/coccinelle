377a7b6b4ba8077f7d5b14821351f15887623a52
Thierry Martinez <martinez@nsup.org>
2016-06-17 14:45:23 +0200

    Fixed: Python exceptions on type errors in iteration.py
    The methods of the class Iteration now raise exceptions when the
    arguments are not of the correct type. Formerly, errors were postponed
    to the next execution of the Python interpreter, which led to unclear
    error messages, as noticed by Luis R. Rodriguez.

--- python/coccilib/iteration.py
+++ python/coccilib/iteration.py
@@ -1,4 +1,17 @@
 from coccinelle import Cocci
+from sys import version_info
+
+# from "six" library and
+# http://stackoverflow.com/questions/4843173/how-to-check-if-type-of-a-variable-is-string-in-python
+PY3 = version_info[0] == 3
+
+if PY3:
+    string_types = str
+else:
+    string_types = basestring
+
+def string_check(value):
+    return isinstance(value, string_types)
 
 class Iteration:
     def __init__(self):
@@ -8,17 +21,27 @@ class Iteration:
         self.__extend_virtual_ids = False
 
     def set_files(self, files):
+        if not (isinstance(files, list)
+                and all(string_check(value) for value in files)):
+            raise TypeError("Iteration.set_files expects a list of strings")
         self.__files = files
 
     def add_virtual_rule(self, rule):
+        if not (string_check(rule)):
+            raise TypeError("Iteration.add_virtual_rule expects a string")
         self.__virtual_rules.add(rule)
 
     def add_virtual_identifier(self, ident, value):
+        if not (string_check(ident) and string_check(value)):
+            raise TypeError(
+                "Iteration.add_virtual_identifier expects string arguments")
         if ident in self.__virtual_identifiers:
             raise ValueError("multiple values specified for {}".format(ident))
         self.__virtual_identifiers[ident] = value
 
     def set_extend_virtual_ids(self, value):
+        if not (string_check(value)):
+            raise TypeError("Iteration.set_extend_virtual_ids expects a string")
         self.__extend_virtual_ids(value)
 
     def register(self):
