c01611882858d0821c877911474bbbe015db5f26
Julia Lawall <Julia.Lawall@lip6.fr>
2017-06-03 08:33:59 +0200

    undo previous two changes which were not type correct

--- engine/cocci_vs_c.ml
+++ engine/cocci_vs_c.ml
@@ -4790,8 +4790,8 @@ let rec (rule_elem_node: (A.rule_elem, F.node) matcher) =
         | None -> expression
         | Some pos ->
             (fun ea eb ->
-              let (min,max) =
-                lin_col_by_pos (Lib_parsing_c.ii_of_expr eb) in
+              let (max,min) =
+                Lib_parsing_c.max_min_by_pos (Lib_parsing_c.ii_of_expr eb) in
               let keep = A.Unitary in
               let inherited = false in
 	      let max_min _ = failwith "no pos" in
