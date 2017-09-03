1c40807f25481f55e4cf7a791ac510f16ba2ff60
julia <julia.lawall@lip6.fr>
2017-09-03 10:00:52 +0200

    stay within 80 chars

--- parsing_c/parsing_recovery_c.ml
+++ parsing_c/parsing_recovery_c.ml
@@ -100,7 +100,8 @@ and find_next_synchro_orig next already_passed =
 
   | (Parser_c.TCBrace i as v)::xs
     when TH.col_of_tok v = 0 && Ast_c.is_origintok i (*skip } in macro*) ->
-      pr2_err ("ERROR-RECOV: found sync '}' at line "^string_of_int (TH.line_of_tok v));
+      pr2_err ("ERROR-RECOV: found sync '}' at line "^
+	       string_of_int (TH.line_of_tok v));
 
       (match xs with
       | [] -> raise (Impossible 94) (* there is a EOF token normally *)
