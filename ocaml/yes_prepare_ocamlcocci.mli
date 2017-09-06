59fe582ccf9746eb4f270236a18d240c550769bf
Thierry Martinez <thierry.martinez@inria.fr>
2017-09-06 16:12:36 +0200

    Reference to stdlib sigs

--- ocaml/yes_prepare_ocamlcocci.mli
+++ ocaml/yes_prepare_ocamlcocci.mli
@@ -28,38 +28,7 @@ val prepare_constraint :
 val prepare : 'a -> Ast_cocci.rule list -> string option
 val prepare_simple : string -> string
 val find_cmifile : string -> string
-module ModuleSet :
-  sig
-    type elt = String.t
-    type t = Set.Make(String).t
-    val empty : t
-    val is_empty : t -> bool
-    val mem : elt -> t -> bool
-    val add : elt -> t -> t
-    val singleton : elt -> t
-    val remove : elt -> t -> t
-    val union : t -> t -> t
-    val inter : t -> t -> t
-    val diff : t -> t -> t
-    val compare : t -> t -> int
-    val equal : t -> t -> bool
-    val subset : t -> t -> bool
-    val iter : (elt -> unit) -> t -> unit
-    val map : (elt -> elt) -> t -> t
-    val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
-    val for_all : (elt -> bool) -> t -> bool
-    val exists : (elt -> bool) -> t -> bool
-    val filter : (elt -> bool) -> t -> t
-    val partition : (elt -> bool) -> t -> t * t
-    val cardinal : t -> int
-    val elements : t -> elt list
-    val min_elt : t -> elt
-    val max_elt : t -> elt
-    val choose : t -> elt
-    val split : elt -> t -> t * bool * t
-    val find : elt -> t -> elt
-    val of_list : elt list -> t
-  end
+module ModuleSet : Set.S with type elt = string
 val approx_coccilib_deps : string -> (string, unit) Hashtbl.t
 val filter_dep :
   (string, 'a) Hashtbl.t ->
