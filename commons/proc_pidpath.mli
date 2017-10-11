e7bec814ae1358add86f0a8381b876086f1c3625
Thierry Martinez <thierry.martinez@inria.fr>
2017-10-12 01:31:38 +0200

    Portable proc_pidpath

--- commons/proc_pidpath.mli
+++ commons/proc_pidpath.mli
@@ -1,3 +1,9 @@
 external proc_pidpath: int -> string = "proc_pidpath_wrapper"
+(** On Mac OS, returns the executable path of the given PID.
+    Returns the empty string on other operating systems. *)
 
 val get_executable_path: unit -> string
+(** Returns the executable path of the current process. *)
+
+val realpath: string -> string
+(** Expands symbolic links in a path. *)
--- commons/proc_pidpath_stubs.c
+++ commons/proc_pidpath_stubs.c
@@ -4,14 +4,18 @@
 #include <caml/fail.h>
 #include <caml/alloc.h>
 #include <unistd.h>
+#ifdef __APPLE__
 #include <libproc.h>
+#endif
 #include <errno.h>
 #include <string.h>
+#include <limits.h>
 
 CAMLprim value
 proc_pidpath_wrapper(value pid_ocaml)
 {
     CAMLparam1(pid_ocaml);
+#ifdef __APPLE__
     int pid = Int_val(pid_ocaml);
     int ret;
     char pathbuf[PROC_PIDPATHINFO_MAXSIZE];
@@ -19,5 +23,21 @@ proc_pidpath_wrapper(value pid_ocaml)
     if (ret <= 0) {
         failwith(strerror(errno));
     }
-    return caml_copy_string(pathbuf);
+#else
+    char *pathbuf = "";
+#endif
+    CAMLreturn(caml_copy_string(pathbuf));
+}
+
+
+CAMLprim value
+realpath_wrapper(value path_ocaml)
+{
+    CAMLparam1(path_ocaml);
+    char pathbuf[PATH_MAX];
+    char *path = String_val(path_ocaml);
+    if (realpath(path, pathbuf) == NULL) {
+        failwith(strerror(errno));
+    }
+    CAMLreturn(caml_copy_string(pathbuf));
 }
