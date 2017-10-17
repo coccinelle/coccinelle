6acc68b3314c6797e0a1cc5ac994637c4333bb81
Julia Lawall <Julia.Lawall@lip6.fr>
2017-10-17 11:55:19 +0200

    format to string without passing through a file

--- commons/common.ml
+++ commons/common.ml
@@ -6009,25 +6009,16 @@ let with_pr2_to_string f =
 
 (* julia: convert something printed using format to print into a string *)
 let format_to_string f =
-  let (nm,o) = Filename.open_temp_file "format_to_s" ".out" in
-  Format.set_formatter_out_channel o;
+  let acc = ref [] in
+  let fns = Format.get_formatter_output_functions() in
+  Format.set_formatter_output_functions
+    (fun s p n -> acc := String.sub s p n :: !acc)
+    (fun _ -> ());
   let _ = f() in
   Format.print_newline();
   Format.print_flush();
-  Format.set_formatter_out_channel stdout;
-  close_out o;
-  let i = open_in nm in
-  let lines = ref [] in
-  let rec loop _ =
-    let cur = input_line i in
-    lines := cur :: !lines;
-    loop() in
-  (try loop() with End_of_file -> ());
-  close_in i;
-  Sys.remove nm;
-  String.concat "\n" (List.rev !lines)
-
-
+  Format.set_formatter_output_functions fns;
+  String.concat "" (List.rev !acc)
 
 (*****************************************************************************)
 (* Misc/test *)
