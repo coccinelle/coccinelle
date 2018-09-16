7cc7f5651322add8cd90d2e33520c31c842efd72
julia <julia.lawall@lip6.fr>
2018-09-16 22:02:09 +0200

    put danger start on the first non-fake node; want all other nodes to be non dangerous for removal

--- parsing_c/danger.ml
+++ parsing_c/danger.ml
@@ -65,7 +65,9 @@ let add_danger xs =
 	Visitor_c.vk_decl (update_danger danger) decl;
 	xs +> List.iter undanger_onedecl;
 	let (max,min) =
-	  Lib_parsing_c.max_min_ii_by_pos(Lib_parsing_c.ii_of_decl decl) in
+	  Lib_parsing_c.max_min_ii_by_pos_filtered
+	    (fun x -> not(Ast_c.is_fake x))
+	    (Lib_parsing_c.ii_of_decl decl) in
 	danger_start min;
 	danger_end max
     | _ -> k decl in
