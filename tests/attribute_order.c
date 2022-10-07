3cc4fad11061a8fcfae759924d72d4d04e344950
Julia Lawall <Julia.Lawall@lip6.fr>
2022-10-07 14:56:00 +0200

    test that attributes are matched in the right place in a ** type

---
+++ tests/attribute_order.c
@@ -0,0 +1,3 @@
+int __att1 * __att2 * __att3 foo() { return 0; }
+int __att1 * __att3 * __att2 nochange1() { return 0; }
+int __att3 * __att1 * __att2 nochange2() { return 0; }
---
+++ tests/attribute_order.cocci
@@ -0,0 +1,5 @@
+@@
+attribute name __att1, __att2, __att3;
+@@
+
+- int __att1 * __att2 * __att3 foo() { return 0; }
