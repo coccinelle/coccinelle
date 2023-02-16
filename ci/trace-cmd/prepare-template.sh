4aa93b3550d099740befd48de57664dfa3046c87
Thierry Martinez <Thierry.Martinez@inria.fr>
2023-02-16 15:11:51 +0100

    Build trace-cmd from git repository

--- ci/trace-cmd/prepare-template.sh
+++ ci/trace-cmd/prepare-template.sh
@@ -4,8 +4,27 @@ sudo tee /etc/apt/apt.conf.d/90forceyes <<EOF
 APT::Get::Assume-Yes "true";
 EOF
 sudo apt-get update
-sudo apt-get install autoconf automake unzip aspcud opam trace-cmd rsync git \
-  mercurial darcs build-essential sudo vim curl libpython3-dev
+sudo apt-get install autoconf automake unzip aspcud opam rsync git \
+    mercurial darcs build-essential sudo vim curl flex python3-dev \
+    libpython3-dev pkg-config zlib1g-dev libzstd-dev swig libaudit-dev
+(
+    git clone https://git.kernel.org/pub/scm/libs/libtrace/libtraceevent.git/
+    cd libtraceevent
+    make
+    sudo make install
+)
+(
+    git clone https://git.kernel.org/pub/scm/libs/libtrace/libtracefs.git/
+    cd libtracefs
+    make
+    sudo make install
+)
+(
+    git clone https://git.kernel.org/pub/scm/utils/trace-cmd/trace-cmd.git/
+    cd trace-cmd
+    make PYTHON_VERS=python3
+    sudo make install PYTHON_VERS=python3
+)
 opam init --auto-setup
 opam switch create 5.0.0
 opam install --yes ocamlfind stdcompat pyml menhir parmap
