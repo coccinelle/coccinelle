4317ba0d61604a711fd8c14646a6809ad81764d8
Michele Martone <michelemartone@users.sourceforge.net>
2023-10-19 12:36:27 +0200

    add comments to 'new' example file

--- tests/new.cpp
+++ tests/new.cpp
@@ -4,16 +4,27 @@ class Ball
 int main()
 {
 
+  // new (placement_params) (type) [initializer]
   new (1) (int) (1);
   new (1) (int) {1};
   new (1) (int);
+
+  // new (placement_params) type [initializer]
   new (1) int (1);
   new (1) int {1};
   new (1) int;
+
+  // new (type)
+  new (int);
+
+  // new (type) initializer
   new (int) (1);
   new (int) {1};
-  new (int);
+
+  // new type
+  new int;
+
+  // new type [initializer]
   new int (1);
   new int {1};
-  new int;
 }
