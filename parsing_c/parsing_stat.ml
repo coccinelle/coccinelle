d78a0bbe0ddf0937eeb17346d6102c890a977ebf
Julia Lawall <Julia.Lawall@inria.fr>
2024-02-16 22:48:41 +0100

    don't print message for every example

--- parsing_c/parsing_stat.ml
+++ parsing_c/parsing_stat.ml
@@ -158,23 +158,23 @@ let print_recurring_problematic_tokens xs =
           (fun (old, example)  -> old + 1, example)
           (fun() -> 0, (file, line_error)) h;
       )));
-  Common.hash_to_list h
-  +> List.sort (fun (k1,(v1,_)) (k2,(v2,_)) -> compare v2 v1)
-  +> Common.take_safe 10
-  +> List.iter (fun (k,(i, (file_ex, line_ex))) ->
-    pr2 (spf "%s: present in %d parsing errors" k i);
-    pr2 ("example: ");
-    let lines = lines_around_error_line ~context:2 (file_ex, line_ex) in
-    if lines != []
-    then
-      begin
-	pr2_xxxxxxxxxxxxxxxxx();
-	pr2 ("maybe 10 most problematic tokens");
-	pr2_xxxxxxxxxxxxxxxxx();
-	lines +> List.iter (fun s -> pr2 ("       " ^ s))
-      end;
-
-  );
+  let infos =
+    Common.hash_to_list h
+      +> List.sort (fun (k1,(v1,_)) (k2,(v2,_)) -> compare v2 v1)
+      +> Common.take_safe 10 in
+  (if infos != []
+  then
+    begin
+      pr2_xxxxxxxxxxxxxxxxx();
+      pr2 ("maybe 10 most problematic tokens");
+      pr2_xxxxxxxxxxxxxxxxx();
+      infos +> List.iter (fun (k,(i, (file_ex, line_ex))) ->
+	pr2 (spf "%s: present in %d parsing errors" k i);
+	pr2 ("example: ");
+	let lines = lines_around_error_line ~context:2 (file_ex, line_ex) in
+	if lines != []
+	then lines +> List.iter (fun s -> pr2 ("       " ^ s)))
+    end);
   pr2_xxxxxxxxxxxxxxxxx();
   ()
 
