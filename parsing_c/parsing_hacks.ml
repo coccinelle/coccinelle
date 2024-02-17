7a7e073490bf530a56023b0ca2f8c216e1f86f22
Julia Lawall <julia.lawall@inria.fr>
2024-02-17 13:47:53 +0100

    more informative error

--- parsing_c/parsing_hacks.ml
+++ parsing_c/parsing_hacks.ml
@@ -3279,8 +3279,8 @@ let rec choose_qualtype toks =
 	then
 	  failwith
 	    (Printf.sprintf
-	       "%s:%d.%d: localacc should be empty at unexpected qualifier token"
-	       (TH.file_of_tok x) (TH.line_of_tok x) (TH.col_of_tok x))
+	       "%s:%d.%d: localacc should be empty at unexpected qualifier token %s"
+	       (TH.file_of_tok x) (TH.line_of_tok x) (TH.col_of_tok x) (TH.str_of_tok x))
 	else loop false false [] (x::acc) xs
     | [] ->
 	if localacc <> []
