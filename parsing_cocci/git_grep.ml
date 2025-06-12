c717dff248a7fcc308c007bf3c8a705cd2c39c26
Julia Lawall <Julia.Lawall@inria.fr>
2025-06-12 13:29:28 +0200

    More precise exclusion of .in files
    Commit e0b6c3ae0 tried to exclude .in.h and .in.c files using grep -v .in.
    But this excludes any file containing the letters "in" with at least one
    preceding character.
    
    This implements what seems to be the intention of that commit by searching
    for files that end in .in.h and .in.c explicitly.
    
    Reported-By: Erick Karanja <karanja99erick@gmail.com>

--- parsing_cocci/git_grep.ml
+++ parsing_cocci/git_grep.ml
@@ -9,10 +9,22 @@ exception Failed
 let interpret dir query suffixes =
   let collect query =
     let cmd =
-      Printf.sprintf "cd %s > /dev/null; git grep -l -w %s -- %s | grep -v .in" dir query suffixes in
+      Printf.sprintf "cd %s > /dev/null; git grep -l -w %s -- %s" dir query suffixes in
     let (res,code) = Common.cmd_to_list_and_status cmd in
     if code = Unix.WEXITED 0 || code = Unix.WEXITED 1
-    then res
+    then
+      List.rev
+	(List.fold_left
+	   (fun prev f ->
+	     let len = String.length f in
+	     if len < 4
+	     then f :: prev
+	     else
+	       let trailing = String.sub f (len - 4) 4 in
+	       if List.mem trailing ["in.c";"in.h"]
+	       then prev
+	       else f :: prev)
+	   [] res)
     else raise Failed in
   try
     match List.map collect query with
